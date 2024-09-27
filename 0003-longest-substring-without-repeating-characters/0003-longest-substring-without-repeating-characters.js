e0a632b53f714cbd790ee5e77dbfa658b458a388
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2024-09-26 17:18:19 -0700

    Time: 70 ms (93.85%), Space: 55.5 MB (32.45%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,103 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    // for ( let i=0; i<s.length; i++ ) {
+    //     let subStr = ""
+    //     for (let j=i; j<=s.length; j++ ) {
+    //         subStr = subStr + (s.charAt(j));
+    //     }
+    //     console.log(subStr)
+
+    // }
+
+    // let l = 0, r = 0, maxlen = 0;
+    // let hashMap = new Map();
+
+    // while ( r < s.length ) {
+    //     if ( hashMap.has(s[r]) ) {
+    //         let value = hashMap.get(s[r]);
+    //         value++;
+    //         hashMap.set(s[r], value);
+    //     } else {
+    //         hashMap.set(s[r], 1)
+    //     }
+
+    //     if ( hashMap.get(s[r]) > 1 ) {
+    //         let value = hashMap.get(s[l]);
+    //         value--;
+    //         hashMap.set(s[l], value);
+    //         l++;
+    //     }
+
+    //     if ( hashMap.size > 0 ) {
+    //         maxlen = Math.max(maxlen, r-l+1);
+    //     }
+    //     r++
+    // }
+    // return maxlen
+
+
+
+
+    // let left=0, right=0, maxlen=0;
+    // let hashMap = new Map();
+
+    // while ( right < s.length ) {
+    //     if ( hashMap.has(s[right]) ) {
+    //         left = Math.max(hashMap.get(s[right])+1, left);
+    //     } // once repeating character is encountered, shrink the window by moving a index+1 of the repeating character
+    //     hashMap.set(s[right], right); // change the map-value of the repeating character to the current encountered value
+        
+    //     maxlen = Math.max(maxlen, right-left+1);
+    //     right++
+    // }
+    // return maxlen
+
+
+
+//   let l = 0, r = 0, maxlen = 0;
+//   let mySet = new Set();
+
+//   while ( r < s.length ) {
+//     while ( mySet.has(s[r]) ) {
+//         mySet.delete(s[l]);
+//         l++;
+//     }
+//     mySet.add(s[r]);
+//     maxlen = Math.max(maxlen, r-l+1);
+//     r++;
+//   }
+//   return maxlen;
+
+
+let l=0, r=0, maxlen=0;
+let mySet = new Set();
+
+while(r<s.length) {
+    while(mySet.has(s[r])) {
+        mySet.delete(s[l])
+        l++
+    }
+    mySet.add(s[r]);
+    maxlen = Math.max(maxlen, r-l+1)
+    r++
+}
+
+return maxlen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
