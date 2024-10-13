dc3ec5ecdd8377f84bc0a0b034c2aa71bb416bfc
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2024-10-13 14:59:33 -0700

    Time: 5 ms (62.78%), Space: 68.8 MB (26.06%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseLL(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null, front = null;
+        while ( temp != null ) {
+            front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        
+        // finding the middle - tortoise-harise pointer 
+        ListNode slow = head, fast = head;
+        while ( fast.next != null && fast.next.next != null ) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // slow gives the middle of linked list
+
+        // reverse half of the linkedlist
+        ListNode first = head;
+        ListNode second = reverseLL(slow.next);
+
+        while ( second != null ) {
+            if ( first.val != second.val ) return false;
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+
+    }
+}
