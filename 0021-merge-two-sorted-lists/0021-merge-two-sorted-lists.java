5484ac041ab8984fd27ea8af44341cd98d0e5ef2
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2024-10-24 11:20:00 -0700

    Time: 0 ms (100%), Space: 42.3 MB (75.43%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -11,34 +11,47 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
 
-        ListNode temp1 = head1, temp2 = head2;
-        ListNode temp = new ListNode(-1);
-        ListNode tempHead = temp;
+// Recursive approach
 
-        while ( temp1 != null && temp2 != null ) {
-            if ( temp1.val <= temp2.val ) {
-                temp.next = temp1;
-                temp1 = temp1.next;
-            } else {
-                temp.next = temp2;
-                temp2 = temp2.next;
-            }
-            temp = temp.next;
+        if ( head1 == null ) return head2;
+        if ( head2 == null ) return head1;
+        if ( head1.val <= head2.val ) {
+            head1.next = mergeTwoLists(head1.next, head2);
+            return head1;
+        } else {
+            head2.next = mergeTwoLists(head1, head2.next);
+            return head2;
         }
 
-        while ( temp1 != null ) {
-            temp.next = temp1;
-            temp1 = temp1.next;
-            temp = temp.next;
-        }
+// Iterative approach
+        // ListNode temp1 = head1, temp2 = head2;
+        // ListNode temp = new ListNode(-1);
+        // ListNode tempHead = temp;
 
-        while ( temp2 != null ) {
-            temp.next = temp2;
-            temp2 = temp2.next;
-            temp = temp.next;
-        }
+        // while ( temp1 != null && temp2 != null ) {
+        //     if ( temp1.val <= temp2.val ) {
+        //         temp.next = temp1;
+        //         temp1 = temp1.next;
+        //     } else {
+        //         temp.next = temp2;
+        //         temp2 = temp2.next;
+        //     }
+        //     temp = temp.next;
+        // }
 
-        return tempHead.next;
-        
+        // while ( temp1 != null ) {
+        //     temp.next = temp1;
+        //     temp1 = temp1.next;
+        //     temp = temp.next;
+        // }
+
+        // while ( temp2 != null ) {
+        //     temp.next = temp2;
+        //     temp2 = temp2.next;
+        //     temp = temp.next;
+        // }
+
+        // return tempHead.next;
+
     }
 }
