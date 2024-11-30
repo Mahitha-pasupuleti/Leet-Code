cbcaf1e93ac21bf1d6c7c95332a8be7d2f78af6e
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2024-11-30 13:46:28 -0800

    Time: 1 ms (99.85%), Space: 46.6 MB (7.15%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,28 +1,37 @@
 class Solution {
     public int singleNumber(int[] nums) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int result = 0;
 
         for ( int i=0; i<nums.length; i++ ) {
-            if ( map.get(nums[i]) == null ) {
-                map.put(nums[i], 1);
-            } else {
-                int value = map.get(nums[i]);
-                map.put(nums[i], ++value);
-            }
+            result = result^nums[i];
         }
 
-        Iterator<HashMap.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        return result;
 
-        while ( iterator.hasNext() ) {
-            HashMap.Entry<Integer, Integer> entry = iterator.next();
-            Integer key = entry.getKey();
-            Integer value = entry.getValue();
-            // System.out.println("Key=" + key + ", Value=" + value);
-            if ( value == 1 ) return key;
-        }
+        // HashMap<Integer, Integer> map = new HashMap<>();
 
-        // System.out.println(map);
-        return 0;
+        // for ( int i=0; i<nums.length; i++ ) {
+        //     if ( map.get(nums[i]) == null ) {
+        //         map.put(nums[i], 1);
+        //     } else {
+        //         int value = map.get(nums[i]);
+        //         map.put(nums[i], ++value);
+        //     }
+        // }
+
+        // Iterator<HashMap.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+
+        // while ( iterator.hasNext() ) {
+        //     HashMap.Entry<Integer, Integer> entry = iterator.next();
+        //     Integer key = entry.getKey();
+        //     Integer value = entry.getValue();
+        //     // System.out.println("Key=" + key + ", Value=" + value);
+        //     if ( value == 1 ) return key;
+        // }
+
+        // // System.out.println(map);
+        // return 0;
+
     }
 }
