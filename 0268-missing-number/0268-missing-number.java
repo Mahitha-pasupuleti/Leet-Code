059182817014e75a903bc0eddaf6dd37c50fd2a7
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2024-12-03 15:33:35 -0800

    Time: 0 ms (100%), Space: 45.8 MB (9.33%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,30 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int start = 0, end = nums.length;
-        int mid = 0;
 
-        while ( start < end ) {
-            mid = start + ( end - start ) / 2;
-            if ( nums[mid] > mid ) end = mid;
-            else start = mid+1;
+        int n = nums.length;
+        // System.out.println(n);
+        int sumOfN = (n*(n+1))/2;
+        int sumOfNums = 0;
+
+        for ( int i=0; i<nums.length; i++ ) {
+            sumOfNums += nums[i];
         }
 
-        return start;
+        // System.out.println(sumOfN + " " + sumOfNums);
+
+        return sumOfN-sumOfNums;
+
+        // Binary search
+        // Arrays.sort(nums);
+        // int start = 0, end = nums.length;
+        // int mid = 0;
+
+        // while ( start < end ) {
+        //     mid = start + ( end - start ) / 2;
+        //     if ( nums[mid] > mid ) end = mid;
+        //     else start = mid+1;
+        // }
+
+        // return start;
     }
 }
