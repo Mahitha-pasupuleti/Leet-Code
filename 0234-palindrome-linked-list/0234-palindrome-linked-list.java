0326c7dc8091492bced0194f75d406555617d348
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-01-23 05:47:54 +0530

    Time: 4 ms (83.67%), Space: 68.6 MB (37.36%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,18 +1,18 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseLL(ListNode head) {
         ListNode temp = head;
         ListNode prev = null, front = null;
-        while ( temp != null ) {
+        while (temp != null) {
             front = temp.next;
             temp.next = prev;
             prev = temp;
@@ -20,11 +20,12 @@ class Solution {
         }
         return prev;
     }
+
     public boolean isPalindrome(ListNode head) {
-        
-        // finding the middle - tortoise-harise pointer 
+
+        // finding the middle - tortoise-hare pointer
         ListNode slow = head, fast = head;
-        while ( fast.next != null && fast.next.next != null ) {
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
@@ -34,8 +35,9 @@ class Solution {
         ListNode first = head;
         ListNode second = reverseLL(slow.next);
 
-        while ( second != null ) {
-            if ( first.val != second.val ) return false;
+        while (second != null) {
+            if (first.val != second.val)
+                return false;
             first = first.next;
             second = second.next;
         }
