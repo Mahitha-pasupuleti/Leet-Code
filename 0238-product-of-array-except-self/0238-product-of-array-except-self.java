f9ae7cd55091345469ef0870a05671084ef04ca3
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-02-21 08:07:03 -0800

    Time: 2 ms (88.03%), Space: 55.4 MB (74.79%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,23 +1,41 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] prefix_product = new int[nums.length+1];
-        int[] suffix_product = new int[nums.length+1];
+        int[] output = new int[nums.length];
 
-        prefix_product[0] = 1;
-        for ( int i=0; i<nums.length; i++ ) {
-             prefix_product[i+1] = prefix_product[i] * nums[i];
+        output[0] = 1;
+        for ( int i=1; i<nums.length; i++ ) {
+           output[i] = output[i-1] * nums[i-1];
         }
-
-        suffix_product[nums.length] = 1;
+    
+        int suffix = 1;
         for ( int i=nums.length-1; i>=0; i-- ) {
-             suffix_product[i] = suffix_product[i+1] * nums[i];
+           output[i] = output[i] * suffix;
+           suffix *= nums[i];
         }
-
-        for ( int i=1; i<=nums.length; i++ ) {
-             nums[i-1] = prefix_product[i-1] * suffix_product[i];
-        }
-
-        return nums;
-
+        return output;
     }
 }
+
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int[] prefix_product = new int[nums.length+1];
+//         int[] suffix_product = new int[nums.length+1];
+
+//         prefix_product[0] = 1;
+//         for ( int i=0; i<nums.length; i++ ) {
+//              prefix_product[i+1] = prefix_product[i] * nums[i];
+//         }
+
+//         suffix_product[nums.length] = 1;
+//         for ( int i=nums.length-1; i>=0; i-- ) {
+//              suffix_product[i] = suffix_product[i+1] * nums[i];
+//         }
+
+//         for ( int i=1; i<=nums.length; i++ ) {
+//              nums[i-1] = prefix_product[i-1] * suffix_product[i];
+//         }
+
+//         return nums;
+
+//     }
+// }
