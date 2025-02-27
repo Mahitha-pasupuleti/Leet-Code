95238be980342ec40679e28e85953d53ccb8d8e8
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-02-27 11:32:57 -0800

    Time: 17 ms (49.65%), Space: 45.6 MB (99.32%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,24 +1,27 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
 
-        int[] result = new int[nums.length];
-        Stack<Integer> stack = new Stack<Integer>();
-        int n = nums.length;
-
-        for ( int i=(2*n)-1; i>=0; i-- ) {
-            while ( !stack.isEmpty() && nums[i%n] >= stack.peek() ) {
-                stack.pop();
-            }
-            if ( stack.isEmpty() ) {
-                result[i%n] = -1;
-            } 
-            else {
-                result[i%n] = stack.peek();
-            }
-            stack.push(nums[i%n]);
+        int[] doubleNums = new int[2 * nums.length];
+        for ( int i=0; i<nums.length; i++ ) {
+            doubleNums[i] = nums[i];
+            doubleNums[i+nums.length] = nums[i];
         }
 
-        return result;
+        Stack<Integer> idxStack = new Stack<>();
+        for ( int i=0; i<doubleNums.length; i++ ) {
+            while ( !idxStack.isEmpty() && doubleNums[idxStack.peek()] < doubleNums[i] ) {
+                doubleNums[idxStack.peek()] = doubleNums[i];
+                idxStack.pop();
+            }
+            idxStack.push(i);
+        }
+
+        while ( !idxStack.isEmpty() ) {
+            doubleNums[idxStack.peek()] = -1;
+            idxStack.pop();
+        }
+
+        return Arrays.copyOfRange(doubleNums, 0, nums.length);
 
     }
 }
