ae3a48117575a0df8590f5ca3a0e3f6890038c19
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-03-06 08:29:15 -0800

    Time: 0 ms (100%), Space: 41.3 MB (39.09%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,40 +14,11 @@
  * }
  */
 class Solution {
-    boolean sametree = true;
-    public void checkSameTree(TreeNode p, TreeNode q) {
-        if ( p == null && q == null ) return;
-        else if ( p == null || q == null ) {
-            sametree = false;
-            return;
-        }
-        if ( p.val != q.val ) sametree = false;
-        checkSameTree(p.left, q.left);
-        checkSameTree(p.right, q.right);
-    }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        checkSameTree(p, q);
-        return sametree;
+        if ( p == null && q == null ) return true;
+        if ( p == null && q != null || p != null && q == null ) return false;
+        if ( p.val != q.val ) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        //return true;
     }
 }
-
-/* 
-class Solution {
-    boolean result = true;
-    public void getTree(TreeNode root1, TreeNode root2) {
-        if ( root1 == null && root2 == null ) {
-            return;
-        } else if ( root1 == null || root2 == null ) {
-            result = false;
-            return;
-        }
-        getTree(root1.left, root2.left);
-        if ( root1.val != root2.val ) result = false;
-        getTree(root1.right, root2.right);
-    }
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-       getTree(p, q);
-       return result;
-    }
-}
-*/
