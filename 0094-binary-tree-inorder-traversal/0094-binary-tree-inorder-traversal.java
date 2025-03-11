c06ade4e4c08f14bcc8708dfd607a04a587d5642
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-03-11 09:58:38 -0700

    Time: 0 ms (100%), Space: 42 MB (8.17%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,15 +14,33 @@
  * }
  */
 class Solution {
-    public void printValues(TreeNode root, List<Integer> result) {
-        if ( root == null ) return;
-        printValues(root.left, result);
-        result.add(root.val);
-        printValues(root.right, result);
-    }
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        printValues(root, result);
+        TreeNode currNode = root;
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> result = new ArrayList();
+        while ( currNode != null || !stack.isEmpty() ) {
+            while ( currNode != null ) {
+                stack.push(currNode);
+                currNode = currNode.left;
+            }
+            currNode = stack.pop();
+            result.add(currNode.val);
+            currNode = currNode.right;
+        }
         return result;
     }
 }
+
+// class Solution {
+//     public void printValues(TreeNode root, List<Integer> result) {
+//         if ( root == null ) return;
+//         printValues(root.left, result);
+//         result.add(root.val);
+//         printValues(root.right, result);
+//     }
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> result = new ArrayList<>();
+//         printValues(root, result);
+//         return result;
+//     }
+// }
