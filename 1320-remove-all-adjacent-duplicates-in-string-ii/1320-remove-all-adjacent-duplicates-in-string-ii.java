9858e3d51f3ef7e6fb983ab63e101690b716d9ca
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-04-01 12:40:28 -0700

    Time: 40 ms (36.35%), Space: 45.9 MB (40.91%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,24 +1,47 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
         Stack<Pair<Character, Integer>> stack = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-            if (!stack.isEmpty() && stack.peek().getKey() == ch) {
+        for ( char ch : s.toCharArray() ) {
+            if ( !stack.isEmpty() && stack.peek().getKey() == ch ) {
                 int count = stack.peek().getValue() + 1;
-                stack.pop(); // Remove the old entry
-                if (count != k) {
+                stack.pop();
+                if ( count != k ) {
                     stack.push(new Pair<>(ch, count));
                 }
             } else {
                 stack.push(new Pair<>(ch, 1));
             }
         }
-
         StringBuilder sb = new StringBuilder();
-        for (Pair<Character, Integer> p : stack) {
-            sb.append(String.valueOf(p.getKey()).repeat(p.getValue()));
+        for ( Pair<Character, Integer> pair : stack ) {
+            sb.append( String.valueOf(pair.getKey()).repeat(pair.getValue()) );
         }
 
         return sb.toString();
     }
 }
+
+// class Solution {
+//     public String removeDuplicates(String s, int k) {
+//         Stack<Pair<Character, Integer>> stack = new Stack<>();
+
+//         for (char ch : s.toCharArray()) {
+//             if (!stack.isEmpty() && stack.peek().getKey() == ch) {
+//                 int count = stack.peek().getValue() + 1;
+//                 stack.pop(); // Remove the old entry
+//                 if (count != k) {
+//                     stack.push(new Pair<>(ch, count));
+//                 }
+//             } else {
+//                 stack.push(new Pair<>(ch, 1));
+//             }
+//         }
+
+//         StringBuilder sb = new StringBuilder();
+//         for (Pair<Character, Integer> p : stack) {
+//             sb.append(String.valueOf(p.getKey()).repeat(p.getValue()));
+//         }
+
+//         return sb.toString();
+//     }
+// }
