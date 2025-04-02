861f7d6570be38c2a3db6db46beea136d143116d
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-04-01 22:07:33 -0700

    Time: 3 ms (26.43%), Space: 43 MB (17.25%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.java
+++ 3447-clear-digits/3447-clear-digits.java
@@ -3,13 +3,13 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         for ( char ch : s.toCharArray() ) {
             // To check if a "ch" is actually a digit use Character.isDigit()
-            if ( Character.isDigit(ch) ) {
+            // Or directly check ch >= '0' && ch <= '9'
+            if ( ch >= '0' && ch <= '9' ) {
                 if ( !stack.isEmpty() ) stack.pop();
             }
             else stack.push(ch);
         }
 
-        // System.out.println(stack);
         StringBuilder sb = new StringBuilder();
         // To iterate through a stack
         for ( char ch : stack ) {
