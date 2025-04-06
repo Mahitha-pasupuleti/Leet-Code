a1e7bbbf119e3e6c086a7aea21189a8a0f81c917
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-04-05 20:13:35 -0700

    Time: 2 ms (83.12%), Space: 44.6 MB (81.59%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,63 +1,18 @@
 class Solution {
-
-    /*
-    public void findCombinations(int[] candidates, int target, int start, List<Integer> templist, List<List<Integer>> list) {
-        if ( target < 0 ) {
-            return;
-        }
-        else if ( target == 0 ) {
-            list.add(new ArrayList<>(templist));
-        } 
+    public void generateSubset(List<List<Integer>> result, List<Integer> subresult, int[] candidates, int target, int remain, int start) {
+        if ( remain == 0 ) result.add(new ArrayList<>(subresult));
+        else if ( remain < 0 ) return;
         else {
             for ( int i=start; i<candidates.length; i++ ) {
-                templist.add(candidates[i]);
-                findCombinations(candidates, target-candidates[i], i, templist, list);
-                templist.remove(templist.size()-1);
+                subresult.add(candidates[i]);
+                generateSubset(result, subresult, candidates, target, remain-candidates[i], i);
+                subresult.remove(subresult.size()-1);
             }
         }
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(candidates);
-
-        findCombinations(candidates, target, 0, new ArrayList<>(), result);
-        return result;
-
-    }
-    */
-
-    public void solve(int start, int n, int[] candidates, int target, int remain
-    , List<Integer> temp, List<List<Integer>> result) {
-
-        if ( remain == 0 ) {
-            result.add(new ArrayList<>(temp));
-            // System.out.println(result);
-            // System.out.println("THere");
-            return;
-        }
-        else if ( remain < 0 ) {
-            // System.out.println("Here");
-            return;
-        }
-        else {
-            for ( int i=start; i<candidates.length; i++ ) {
-                temp.add(candidates[i]);
-                // System.out.println(temp);
-                solve(i, n, candidates, target, remain-candidates[i], temp, result);
-                temp.remove(temp.size()-1);
-            }
-        }
-    }
-
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
-        List<Integer> temp = new ArrayList<>();
-        List<List<Integer>> result = new ArrayList<>();
-        int n = candidates.length;
-        Arrays.sort(candidates);
-
-        solve(0, n, candidates, target, target, temp, result);
+        generateSubset(result, new ArrayList<>(), candidates, target, target, 0);
         return result;
     }
-
 }
