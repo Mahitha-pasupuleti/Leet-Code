0f09953d95fa8e0933b7670e233da0f33799c9a4
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-04-05 21:11:02 -0700

    Time: 4 ms (43.21%), Space: 43.3 MB (72.56%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,34 +1,20 @@
 class Solution {
-
-    public void solve(int start, int n, int[] candidates, int target, int remain,
-    List<Integer> temp, List<List<Integer>> result) {
-        if ( remain == 0 ) {
-            result.add(new ArrayList<>(temp));
-            return;
-        }
-        else if ( remain < 0 ) {
-            return;
-        }
+    public void generateSubset(List<List<Integer>> result, List<Integer> subresult, int[] candidates, int target, int remain, int start) {
+        if ( remain == 0 ) result.add(new ArrayList<>(subresult));
+        else if ( remain < 0 ) return;
         else {
-            for ( int i=start; i<n; i++ ) {
-                if ( i > start && candidates[i] == candidates[i-1] ) continue;
-                temp.add(candidates[i]);
-                solve(i+1, n, candidates, target, remain-candidates[i], temp, result);
-                temp.remove(temp.size()-1);
+            for ( int i=start; i<candidates.length; i++ ) {
+                if ( i>start && candidates[i] == candidates[i-1]) continue;
+                subresult.add(candidates[i]);
+                generateSubset(result, subresult, candidates, target, remain-candidates[i], i+1);
+                subresult.remove(subresult.size()-1);
             }
         }
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
-
-        // for ( int i=0; i<candidates.length; i++ ) {
-        //     System.out.print(candidates[i]);
-        // }
-
-        List<Integer> temp = new ArrayList<>();
         List<List<Integer>> result = new ArrayList<>();
-
-        solve(0, candidates.length, candidates, target, target, temp, result);
+        Arrays.sort(candidates);
+        generateSubset(result , new ArrayList<>(), candidates, target, target, 0);
         return result;
     }
 }
