0dc2552a30ac8ac7c5475cafef00632466383e6b
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-04-09 10:11:10 -0700

    Time: 6 ms (88.6%), Space: 44.1 MB (86.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,46 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] tails = new int[nums.length];
-        int size = 0;
-
-        for ( int x: nums ) {
-            int i=0, j=size;
-
-            while ( i != j ) {
-                int m = ( i + j ) / 2;
-                if ( tails[m] < x ) {
-                    i = m+1;
-                } else {
-                    j = m;
+        List<Integer> result = new ArrayList();
+        for ( int x : nums ) {
+            if ( result.isEmpty() || result.get(result.size()-1) < x ) {
+                result.add(x);
+            } else {
+                int start=0, end=result.size()-1;
+                int swapIndex = 0;
+                while ( start < end ) {
+                    int mid = (start + end)/2;
+                    if ( result.get(mid) < x ) start = mid+1;
+                    else end = mid;
                 }
+                result.set(start, x);
             }
-
-            tails[i] = x;
-            if ( i == size ) ++size;
         }
-
-        return size;
+        return result.size();
     }
 }
+
+// class Solution {
+//     // difficult problem
+//     public int lengthOfLIS(int[] nums) {
+//         int[] tails = new int[nums.length];
+//         int size = 0;
+
+//         for ( int x: nums ) {
+//             int i=0, j=size;
+
+//             while ( i != j ) {
+//                 int m = ( i + j ) / 2;
+//                 if ( tails[m] < x ) {
+//                     i = m+1;
+//                 } else {
+//                     j = m;
+//                 }
+//             }
+
+//             tails[i] = x;
+//             if ( i == size ) ++size;
+//         }
+
+//         return size;
+//     }
+// }
