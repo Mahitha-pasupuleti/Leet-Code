74e3429bfa0bd00f52a343b7216d90f3c904ed66
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-05-20 14:32:50 -0700

    Time: 0 ms (100%), Space: 40.5 MB (48.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,19 @@
 class Solution {
+    public int uniquePaths(int m, int n) {
+        // we need two loops
+        int[][] dp = new int[m][n];
+        for ( int i=0; i<m; i++ ) {
+            for ( int j=0; j<n; j++ ) {
+                if ( i == 0 || j == 0 ) dp[i][j] = 1;
+                else dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
+
+/*
+class Solution {
     int[][] dp;
     public int solve(int m, int n) {
         if ( m == 0 || n == 0 ) return 0; // valid
@@ -14,3 +29,4 @@ class Solution {
         return solve(m, n);
     }
 }
+*/
