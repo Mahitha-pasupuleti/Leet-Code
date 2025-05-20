901dbd9bd6cd9bbb6d94429a1ee22d8d370443f1
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-05-19 17:57:17 -0700

    Time: 1 ms (84.18%), Space: 42 MB (50.47%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,29 +1,32 @@
 class Solution {
-    Map<Integer, Integer> memo = new HashMap<>();
-
+    int[] dp;
     public int solve(String s, int i) {
-        // Successfully reached end
-        if (i == s.length()) return 1;
-        // Leading zero is invalid
-        if (s.charAt(i) == '0') return 0;
+        if ( i >= s.length() ) return 1;
+        if ( dp[i] != -1 ) return dp[i];
 
-        if (memo.containsKey(i)) return memo.get(i);
+        int x=0, y=0;
 
-        int result = solve(s, i + 1); // One-digit decode
+        int curr1Digit = s.charAt(i) - '0'; // automatically between 0 - 9
+        if ( curr1Digit == 0 ) return 0;
+        x += solve(s, i+1);
 
-        // Two-digit decode
-        if (i + 1 < s.length()) {
-            int num = Integer.parseInt(s.substring(i, i + 2));
-            if (num <= 26) {
-                result += solve(s, i + 2);
-            }
+        if ( i+1 < s.length() ) { // valid substring exists
+            int curr2Digits = Integer.parseInt(s.substring(i, i + 2));
+            if ( curr2Digits <= 26 ) y += solve(s, i+2); // need to check explicity if b/w 10 to 26
         }
 
-        memo.put(i, result);
-        return result;
+        return dp[i] = x + y;
     }
-
     public int numDecodings(String s) {
+        int n = s.length();
+        dp = new int[n+1];
+        Arrays.fill(dp, -1);
         return solve(s, 0);
     }
 }
+
+/*
+    - in how many ways can u divide the string so it remains valid
+    - u either divide into 1 or 2
+    - if the value is 0 or starts with 0, discard it we dontr need it 
+*/
