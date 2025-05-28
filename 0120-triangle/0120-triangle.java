4ebdaf809d50e8f5c8e06cb76f4f86fe31909df3
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-05-28 10:07:48 -0700

    Time: 1 ms (99.66%), Space: 44.7 MB (22.43%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solve( int start, int end, int n, List<List<Integer>> triangle, int[][] dp ) {
+        if ( start >= n || end >= n ) return Integer.MAX_VALUE;
+        if ( dp[start][end] != -1 ) return dp[start][end];
+        if ( start == n-1 ) return 0;
+        return dp[start][end] = Math.min( 
+            triangle.get(start+1).get(end) + solve(start+1, end, n, triangle, dp), 
+            triangle.get(start+1).get(end+1) + solve(start+1, end+1, n, triangle, dp) 
+        );
+    }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        for ( int i=0; i<n; i++ ) {
+            Arrays.fill(dp[i], -1);
+        }
+        return triangle.get(0).get(0) + solve(0, 0, n, triangle, dp);
+    }
+}
