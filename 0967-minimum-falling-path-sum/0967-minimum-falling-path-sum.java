427b9d49a5a6017e46827ede966bb261a8a46cdd
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-05-28 13:13:57 -0700

    Time: 3 ms (78.22%), Space: 45.3 MB (77.7%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,26 +1,49 @@
 class Solution {
-    public int solve(int x, int y, int n, int[][] matrix, int[][] dp ) {
-        if ( x < 0 || y < 0 || x >= n || y >= n) return Integer.MAX_VALUE; // invalid path
-        if ( x == n-1 ) return matrix[x][y]; // base condition
-
-        if ( dp[x][y] != Integer.MAX_VALUE ) return dp[x][y];
-        
-        int minVal = Integer.MAX_VALUE;
-        for ( int i = y-1; i<= y+1; i++ ) {
-            minVal = Math.min( minVal, solve(x+1, i, n, matrix, dp) );
-        }
-        return dp[x][y] = matrix[x][y] + minVal;
-    }
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        int minVal = Integer.MAX_VALUE;
         int[][] dp = new int[n][n];
-        for ( int i=0; i<n; i++ ) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        for ( int j=0; j<n; j++ ) {
+            dp[n-1][j] = matrix[n-1][j];
         }
-        for ( int i=0; i<n; i++ ) {
-            minVal = Math.min( minVal, solve(0, i, n, matrix, dp) );
+        for ( int i=n-2; i>=0; i-- ) { // row
+            for ( int j=0; j<n; j++ ) {
+                int minVal = Integer.MAX_VALUE;
+                for ( int k=j-1; k<=j+1; k++ ) {
+                    if ( k >= 0 && k < n ) minVal = Math.min( minVal, dp[i+1][k] ); // next row
+                }
+                dp[i][j] = matrix[i][j] + minVal;
+            }
         }
-        return minVal;
+        int result = Integer.MAX_VALUE;
+        for ( int i=0; i<n; i++ ) result = Math.min( result, dp[0][i] );
+
+        return result;
     }
 }
+
+// class Solution {
+//     public int solve(int x, int y, int n, int[][] matrix, int[][] dp ) {
+//         if ( x < 0 || y < 0 || x >= n || y >= n) return Integer.MAX_VALUE; // invalid path
+//         if ( x == n-1 ) return matrix[x][y]; // base condition
+
+//         if ( dp[x][y] != Integer.MAX_VALUE ) return dp[x][y];
+        
+//         int minVal = Integer.MAX_VALUE;
+//         for ( int i = y-1; i<= y+1; i++ ) {
+//             minVal = Math.min( minVal, solve(x+1, i, n, matrix, dp) );
+//         }
+//         return dp[x][y] = matrix[x][y] + minVal;
+//     }
+//     public int minFallingPathSum(int[][] matrix) {
+//         int n = matrix.length;
+//         int minVal = Integer.MAX_VALUE;
+//         int[][] dp = new int[n][n];
+//         for ( int i=0; i<n; i++ ) {
+//             Arrays.fill(dp[i], Integer.MAX_VALUE);
+//         }
+//         for ( int i=0; i<n; i++ ) {
+//             minVal = Math.min( minVal, solve(0, i, n, matrix, dp) );
+//         }
+//         return minVal;
+//     }
+// }
