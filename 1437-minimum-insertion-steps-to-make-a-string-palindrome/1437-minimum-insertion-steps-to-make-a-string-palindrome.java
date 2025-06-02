f968b0879f917065564478af217a06570946a40c
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-06-02 15:41:21 -0700

    Time: 25 ms (66.46%), Space: 45.5 MB (67.93%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,25 +1,24 @@
 class Solution {
-    public int minInsertions(String s1) {
-        /* 
-        - Find the longest palindrome subsequence of the given string
-        - This can be done by below steps
-          - Perform LCS between s1(main string) and s2(reversed string)
-          - The result is the length of the Longest Palindrome Subsequence
-        - Finally length of original String - length of Longest Palindrome Subsequence is our answer
-        */
+    public int minInsertions(String s) {
+        // leetcode
+        // edocteel
+        // ece
+        StringBuilder sCopy = new StringBuilder(s);
+        sCopy.reverse();
+        String sRev = sCopy.toString();
 
-        StringBuilder str = new StringBuilder(s1);
-        String s2 = str.reverse().toString();
+        int n = s.length();
 
-        int n = s1.length();
-        int m = s2.length();
+        int[][] dp = new int[n + 1][n + 1];
 
-        int[][] dp = new int[n+1][m+1];
+        for ( int i=0; i<=n; i++ ) {
+            dp[i][0] = 0;
+            dp[0][i] = 0;
+        }
 
-        for ( int i=0; i<n+1; i++ ) {
-            for ( int j=0; j<m+1; j++ ) {
-                if ( i==0 || j==0 ) dp[i][j] = 0;
-                else if ( s1.charAt(i-1) == s2.charAt(j-1) ) {
+        for ( int i=1; i<=n; i++ ) {
+            for ( int j=1; j<=n; j++ ) {
+                if ( s.charAt(i-1) == sRev.charAt(j-1) ) {
                     dp[i][j] = 1 + dp[i-1][j-1];
                 } else {
                     dp[i][j] = Math.max( dp[i-1][j], dp[i][j-1] );
@@ -27,6 +26,6 @@ class Solution {
             }
         }
 
-        return n - dp[n][m];
+        return n - dp[n][n];
     }
 }
