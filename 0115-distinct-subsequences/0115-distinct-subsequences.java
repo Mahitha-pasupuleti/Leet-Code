ea645a5a3e74815031eca2d39856d89bff0a3fc1
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-06-02 18:55:44 -0700

    Time: 18 ms (81.4%), Space: 51.4 MB (33.28%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,22 +1,26 @@
 class Solution {
-    private int findNumDistinct(String s, String t, int n, int m, int[][] dp) {
-        if ( m <= 0 ) return 1;
-        if ( n <= 0 ) return 0;
-        if ( dp[n][m] != -1 ) return dp[n][m];
-        if ( s.charAt(n-1) == t.charAt(m-1) ) {
-            return dp[n][m] = findNumDistinct(s, t, n-1, m-1, dp) + findNumDistinct(s, t, n-1, m, dp);
-        }
-        return dp[n][m] = findNumDistinct(s, t, n-1, m, dp);
-    }
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
 
         int[][] dp = new int[n+1][m+1];
+
         for ( int i=0; i<=n; i++ ) {
-            Arrays.fill(dp[i], -1);
+            dp[i][0] = 1; // j = 0
+        }
+        for ( int j=1; j<=m; j++ ) {
+            dp[0][j] = 0; // i = 0
+        }
+        for ( int i=1; i<=n; i++ ) {
+            for ( int j=1; j<=m; j++ ) {
+                if ( s.charAt(i-1) == t.charAt(j-1) ) {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
 
-        return findNumDistinct(s, t, n, m, dp);
+        return dp[n][m];
     }
 }
