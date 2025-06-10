b0e7eadf4406a86a28ed9e04d97170b8822cef43
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-06-10 16:51:50 -0700

    Time: 6 ms (84.19%), Space: 57.3 MB (40.3%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,4 +1,9 @@
 class Solution {
+    // Cyclic Sort
+    // Hint-1 : n where all the integers of nums are in the range [1, n]
+    // Hint-2 : algorithm that runs in O(n) time
+    // Hint-3 : only constant auxiliary space -> no modifications to input array
+    // 
     public void swap(int[] nums, int first, int second) {
         int temp = nums[first];
         nums[first] = nums[second];
