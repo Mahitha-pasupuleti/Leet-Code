d30a70caf6ea9c265cebb2ac169b5b35eb065e65
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-06-10 10:53:12 -0700

    Time: 2 ms (89.16%), Space: 57.8 MB (6.32%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,69 +1,36 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] prefixProduct = new int[n];
-        int[] suffixProduct = new int[n];
+        /*
+            [1, 2, 3, 4]
+            
+            [-1, 1, 0, -3, 3] totalProductWithZero, totalProductWithoutZero
 
-        // Step 1: Compute prefix product
-        prefixProduct[0] = 1;
-        for (int i = 1; i < n; i++) {
-            prefixProduct[i] = prefixProduct[i - 1] * nums[i - 1];
+            [-1, 1, -3, 3] product = 9
+            [-9, 9, -3, 3]
+        */
+
+        int totalProductWithZero = 1;
+        int totalProductWithoutZero = 1;
+        int zeroCount = 0;
+
+        for ( int num : nums ) {
+            totalProductWithZero *= num;
+            if ( num != 0 ) {
+                totalProductWithoutZero *= num;
+            } else {
+                zeroCount++;
+            }
         }
 
-        // Step 2: Compute suffix product
-        suffixProduct[n - 1] = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            suffixProduct[i] = suffixProduct[i + 1] * nums[i + 1];
-        }
-
-        // Step 3: Compute final result by multiplying prefix and suffix
-        for (int i = 0; i < n; i++) {
-            nums[i] = prefixProduct[i] * suffixProduct[i];
+        for ( int i=0; i<nums.length; i++ ) {
+            if ( nums[i] == 0 ) {
+                if ( zeroCount > 1 ) nums[i] = totalProductWithZero;
+                else if ( zeroCount == 1 ) nums[i] = totalProductWithoutZero;
+            } else {
+                nums[i] = totalProductWithZero / nums[i];
+            }
         }
 
         return nums;
     }
 }
-
-
-// class Solution {
-//     public int[] productExceptSelf(int[] nums) {
-//         int[] output = new int[nums.length];
-
-//         output[0] = 1;
-//         for ( int i=1; i<nums.length; i++ ) {
-//            output[i] = output[i-1] * nums[i-1];
-//         }
-    
-//         int suffix = 1;
-//         for ( int i=nums.length-1; i>=0; i-- ) {
-//            output[i] = output[i] * suffix;
-//            suffix *= nums[i];
-//         }
-//         return output;
-//     }
-// }
-
-// class Solution {
-//     public int[] productExceptSelf(int[] nums) {
-//         int[] prefix_product = new int[nums.length+1];
-//         int[] suffix_product = new int[nums.length+1];
-
-//         prefix_product[0] = 1;
-//         for ( int i=0; i<nums.length; i++ ) {
-//              prefix_product[i+1] = prefix_product[i] * nums[i];
-//         }
-
-//         suffix_product[nums.length] = 1;
-//         for ( int i=nums.length-1; i>=0; i-- ) {
-//              suffix_product[i] = suffix_product[i+1] * nums[i];
-//         }
-
-//         for ( int i=1; i<=nums.length; i++ ) {
-//              nums[i-1] = prefix_product[i-1] * suffix_product[i];
-//         }
-
-//         return nums;
-
-//     }
-// }
