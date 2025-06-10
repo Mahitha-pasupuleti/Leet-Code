47be9777180d53cfb6e7247336c91e9052b5eca1
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-06-10 14:47:58 -0700

    Time: 1 ms (35.29%), Space: 45 MB (96.79%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,30 +1,26 @@
 class Solution {
+    public void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
     public int missingNumber(int[] nums) {
-
-        int n = nums.length;
-        // System.out.println(n);
-        int sumOfN = (n*(n+1))/2;
-        int sumOfNums = 0;
-
-        for ( int i=0; i<nums.length; i++ ) {
-            sumOfNums += nums[i];
+        // Using cyclic sort
+        int currentIndex = 0, n = nums.length;
+        while ( currentIndex < n ) {
+            int correctIndex = nums[currentIndex];
+            // If element is at the wrong index - nums[i] != nums[correctIndex]
+            if ( correctIndex < n && nums[currentIndex] != nums[correctIndex] ) {
+                swap( nums, currentIndex, correctIndex);
+            } else {
+                currentIndex++;
+            }
         }
 
-        // System.out.println(sumOfN + " " + sumOfNums);
+        for ( int i=0; i<n; i++ ) {
+            if ( i != nums[i] ) return i;
+        }
 
-        return sumOfN-sumOfNums;
-
-        // Binary search
-        // Arrays.sort(nums);
-        // int start = 0, end = nums.length;
-        // int mid = 0;
-
-        // while ( start < end ) {
-        //     mid = start + ( end - start ) / 2;
-        //     if ( nums[mid] > mid ) end = mid;
-        //     else start = mid+1;
-        // }
-
-        // return start;
+        return n;
     }
 }
