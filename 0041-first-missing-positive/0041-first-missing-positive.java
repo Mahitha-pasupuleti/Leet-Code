bada796af6aa820b441a4a7b357b80cc9e0d5b4d
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-06-10 17:11:19 -0700

    Time: 1 ms (100%), Space: 57.4 MB (40.11%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,4 +1,9 @@
 class Solution {
+    
+    // Cyclic Sort
+    // Hint-1 : algorithm that runs in O(n)
+    // Hint-2 : O(1) auxiliary space
+
     public void swap(int[] nums, int first, int second) {
         int temp = nums[first];
         nums[first] = nums[second];
