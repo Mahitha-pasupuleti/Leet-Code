e72c75779f74069a0895c8945753b2d5237b2c1b
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-06-11 12:00:39 -0700

    Time: 432 ms (19.56%), Space: 53.3 MB (10.85%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
-        int i=0, j, k;
-        List<List<Integer>> myResult = new ArrayList();
+        /*
+         [-1,0,1,2,-1,-4]
+         [-1,-1,0,1,2,4]
+         -1 -> get (0, 1), (2, -1)
+          0 -> get ( 1, -1)
+          distinct triplets : use set
+        */
 
         Arrays.sort(nums);
 
-        while ( i < n-2 ) {
-            j = i+1; 
-            k = n-1;
-            while ( j < k ) {
-                int currentSum = nums[j] + nums[k];
-                if ( currentSum == 0-nums[i] ) {
-                    myResult.add(List.of(nums[i], nums[j], nums[k]));
-                    while ( j < k && nums[j] == nums[j+1] ) j++;
-                    while ( j < k && nums[k] == nums[k-1] ) k--;
-                    j++;
-                    k--;
-                } 
-                else if ( currentSum < 0-nums[i] ) j++;
-                else k--;
+        int n = nums.length;
+        Set<List<Integer>> tripletSet = new HashSet<>();
+        for ( int i=0; i<n-2; i++ ) { // i=1
+            if ( i>0 && nums[i] == nums[i-1] ) continue;
+            int currVal = nums[i]; // -1, need to find pairs with target 1 // 0
+            int target = 0 - currVal; // 0
+            Map<Integer, Integer> valueIndexMap = new HashMap<>();
+            for ( int j=i+1; j<n; j++ ) { // j = 4
+                int diff = target - nums[j]; // -2
+                if ( valueIndexMap.containsKey( diff ) ) {
+                    tripletSet.add( Arrays.asList(nums[i], diff, nums[j]) );
+                }
+                valueIndexMap.put(nums[j], j); // [1:2], [2:3]
             }
-            while ( i < n-2 && nums[i] == nums[i+1] ) i++;
-            i++;
         }
-
-        return myResult;
-
+        // System.out.println(tripletSet);
+        return new ArrayList<>(tripletSet);
     }
 }
