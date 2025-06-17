c97ebe15948f83c10b0f999b258e88abff0f5fcf
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-06-17 15:17:09 -0700

    Time: 7 ms (42.48%), Space: 45.1 MB (97.44%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,54 +1,36 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-
         Stack<Integer> stack = new Stack<>();
-
-        for ( int i=0; i<asteroids.length; i++ ) {
-            // int[] value = new int[2];
-            // value[0] = Math.abs(asteroids[i]);
-            // value[1] = ( asteroids[i] >= 0 ) ? 1 : -1;
-            // for ( int j=0; j<value.length; j++ ) {
-            //     System.out.println(value[j]);
-            // }
-
+        for ( int asteriod : asteroids ) {
             boolean destroyed = false;
-
-            // if ( stack.isEmpty() ) {
-            //     stack.push(value);
-            // }
-            // else if ( !stack.isEmpty() && value[1] == -1 ) {
-            while ( !stack.isEmpty() && asteroids[i]<0 && stack.peek()>0 ) {
-                if ( stack.peek() < Math.abs(asteroids[i]) ) {
-                    stack.pop();
-                } else if ( stack.peek() == Math.abs(asteroids[i]) ) {
-                    stack.pop();
-                    destroyed = true;
-                    break;
-                } else {
-                    destroyed = true;
-                    break;
+            if ( !stack.isEmpty() && stack.peek()>0 && asteriod<0 ) {
+                while ( !stack.isEmpty() && stack.peek()>0 && asteriod<0 ) {
+                    int top = stack.peek();
+                    if ( top > Math.abs(asteriod) ) {
+                        destroyed = false;
+                        break;
+                    }
+                    else if ( top < Math.abs(asteriod) ) {
+                        stack.pop();
+                        destroyed = true;
+                    } 
+                    else if ( top == Math.abs(asteriod) ) {
+                        stack.pop();
+                        destroyed = false;
+                        break;
+                    }
                 }
-            }
-            if ( !destroyed ) {
-                stack.push(asteroids[i]);
+                if ( destroyed == true ) stack.push( asteriod );
+            } else {
+                stack.push( asteriod );
             }
         }
 
-        // Iterator value = stack.iterator(); 
-        // System.out.println("The iterator values are: "); 
-        // while (value.hasNext()) { 
-        //     System.out.println(value.next()); 
-        // }
-
-        // for (int arr : stack) {
-        //     System.out.println(arr);
-        // }
-
-        int[] result = new int[stack.size()];
-        for ( int i=result.length-1; i>=0; i-- ) {
+        int n = stack.size();
+        int[] result = new int[n];
+        for ( int i=n-1; i>=0; i-- ) {
             result[i] = stack.pop();
         }
-
         return result;
     }
 }
