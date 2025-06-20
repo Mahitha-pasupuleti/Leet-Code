5a7ec014b4e0a5009f2f4d39cdd52eeaa4b3efca
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-06-20 16:52:53 -0700

    Time: 88 ms (5.97%), Space: 58.6 MB (79.74%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,25 +1,18 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
-        int n = temperatures.length;
-        int[] result = new int[n];
-
-        for ( int i=n-1; i>=0; i-- ) {
-            while ( !stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i] ) {
-                stack.pop();
-            }
-            // int[] value = new int[2];
-            // value[0] = temperatures[i];
-            // value[1] = i;
-            if ( !stack.isEmpty() ) {
-                result[i] = stack.peek() - i;
-            }
-            // else if ( temperatures[stack.peek()] > temperatures[i] ) {
-            //     result[i] = stack.peek() - i;
-            // }
-            stack.push(i);
+        Stack<int[]> stack = new Stack<>();
+        for ( int i=0; i<temperatures.length; i++ ) {
+                while ( !stack.isEmpty() && stack.peek()[0] < temperatures[i] ) {
+                    temperatures[ stack.peek()[1] ] = i - stack.peek()[1];
+                    stack.pop();
+                }
+                stack.push( new int[]{ temperatures[i], i } );
         }
-
-        return result;
+        
+        while ( !stack.isEmpty() ) {
+            temperatures[ stack.peek()[1] ] = 0;
+            stack.pop();
+        }
+        return temperatures;
     }
 }
