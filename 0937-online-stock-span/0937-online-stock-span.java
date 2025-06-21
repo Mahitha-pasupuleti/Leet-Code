35713c886e367d0d876648dedf890bcc01406862
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-06-20 17:11:24 -0700

    Time: 32 ms (37.74%), Space: 55.6 MB (40.39%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.java
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -1,21 +1,17 @@
 class StockSpanner {
     Stack<int[]> stack;
-
     public StockSpanner() {
         stack = new Stack<>();
     }
     
     public int next(int price) {
-        int[] newEntry = new int[2];
-        newEntry[0] = price;
-        newEntry[1] = 1;
+        int count = 1;
         while ( !stack.isEmpty() && stack.peek()[0] <= price ) {
-            newEntry[1] += stack.peek()[1];
+            count += stack.peek()[1];
             stack.pop();
         }
-        stack.push(newEntry);
-
-        return newEntry[1];
+        stack.push( new int[]{price, count} );
+        return count;
     }
 }
 
