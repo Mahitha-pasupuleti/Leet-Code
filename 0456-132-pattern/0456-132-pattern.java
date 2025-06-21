b3400940e98e33757099aa0fadf3081d21ad8bdf
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-06-21 16:01:58 -0700

    Time: 40 ms (63.69%), Space: 76.7 MB (5.45%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -1,16 +1,26 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
-        Stack<Integer> stack = new Stack<>();
         int n = nums.length;
-        int max = Integer.MIN_VALUE;
+        if (n < 3) return false;
 
-        for ( int i=n-1; i>=0; i-- ) {
-            if ( nums[i] < max ) return true;
-            while ( !stack.isEmpty() && stack.peek() < nums[i] ) {
-                max = stack.pop();
+        Stack<Integer> stack = new Stack<>();
+        int s3 = Integer.MIN_VALUE; // This will represent the "2" in the 132 pattern
+
+        // Traverse from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            // If we find a number smaller than s3, we found a 132 pattern
+            if (nums[i] < s3) return true;
+
+            // While current number is greater than the top of stack,
+            // update s3 to the popped element (potential nums[k])
+            while (!stack.isEmpty() && nums[i] > stack.peek()) {
+                s3 = stack.pop(); // This popped element is the largest "2" less than current "3"
             }
-            stack.push( nums[i] );
+
+            // Push current number as a potential "3" candidate (nums[j])
+            stack.push(nums[i]);
         }
+
         return false;
     }
 }
