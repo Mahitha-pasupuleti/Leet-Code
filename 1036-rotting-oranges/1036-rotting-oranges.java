dec69d189775dc70660f327af1f9c88c6298001b
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-07-15 16:02:53 -0700

    Time: 1 ms (100%), Space: 42.7 MB (38.74%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,56 +1,44 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        // BFS
-        // neighbours of 2 must become 2 if they are 1 before
-        // find all neighbours of a node(2) at same level whose value is (1)
-        // add all nodes(2) into BFS level 0
-        // chage 1's to 2's
-        // dont touch '0'
+        int n = grid.length, m = grid[0].length;
+        int[][] directions = {{-1,0},{0,1},{0,-1},{1,0}};
+        Queue<int[]> BFS = new ArrayDeque<>();
 
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] directions = { {-1,0}, {1,0}, {0,-1}, {0,1} };
-
-        int levelCount = -1;
         int countOfOranges = 0;
 
-        Queue<int[]> BFS = new LinkedList<>();
-        for ( int i=0; i<m; i++ ) {
-            for ( int j=0; j<n; j++ ) {
-                if ( grid[i][j] == 2 ) BFS.add(new int[]{i, j});
-                else if ( grid[i][j] == 1 ) countOfOranges++;
+        for ( int i=0; i<n; i++ ) {
+            for ( int j=0; j<m; j++ ) {
+                if ( grid[i][j] == 2 ) {
+                    BFS.add(new int[]{i,j});
+                } else if ( grid[i][j] == 1 ) countOfOranges++;
             }
         }
 
-        if ( countOfOranges == 0 ) return 0;
+        if ( countOfOranges == 0 ) return countOfOranges;
 
-        int levelSize = 0;
+        int level = 0;
 
-        while ( !BFS.isEmpty() ) {
-            levelCount++;
-            levelSize = BFS.size();
-            while ( levelSize != 0 ) {
-                levelSize--;
-                int[] node = BFS.poll();
-                int x = node[0], y = node[1];
-
+        while( !BFS.isEmpty() ) {
+            level++;
+            int size = BFS.size();
+            for ( int i=0; i<size; i++ ) {
+                int[] top = BFS.poll();
                 for ( int[] dir : directions ) {
-                    int dx = x + dir[0], dy = y + dir[1];
-                    if ( (dx >= 0 && dx < m) && (dy >= 0 && dy < n) && grid[dx][dy] == 1 ) {
-                        BFS.add(new int[]{dx, dy});
-                        grid[dx][dy] = '2';
-                    }
+                    int dx = top[0] + dir[0];
+                    int dy = top[1] + dir[1];
+                    if ( dx<0 || dy<0 || dx>=n || dy>=m || grid[dx][dy] != 1 ) continue;
+                    grid[dx][dy] = 2;
+                    BFS.add(new int[]{dx, dy});
                 }
             }
         }
 
-        for ( int i=0; i<m; i++ ) {
-            for ( int j=0; j<n; j++ ) {
-                if ( grid[i][j] == 1 ) return -1;
+        for ( int i=0; i<n; i++ ) {
+            for ( int j=0; j<m; j++ ) {
+                if ( grid[i][j] == 1 ) return -1; 
             }
-        }
+        } 
 
-        return levelCount;
-
+        return level-1;
     }
 }
