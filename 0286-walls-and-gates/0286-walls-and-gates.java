58668100b305a2ac84fac9a474770d4585af12fa
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-07-15 15:35:45 -0700

    Time: 10 ms (38.81%), Space: 48.7 MB (17.28%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.java
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -1,50 +1,37 @@
 class Solution {
     public void wallsAndGates(int[][] rooms) {
-        /*
-            Similar to House to Well problem, where instead of "H to W", we choose "W to H"
-            Find shortest distance from 'Gate' to 'Empty room' using BFS
-            Multi-source BFS
-            We need to add all Gate's because, if we wait for one Gate to complete BFS
-            there are high chances 'Empty Rooms' near to other 'Gates' will not be considered
 
-         */
+        int n = rooms.length, m = rooms[0].length;
+        int[][] directions = {{-1,0},{1,0},{0,1},{0,-1}};
+        boolean[][] visited = new boolean[n][m];
+        Queue<int[]> bfs = new ArrayDeque<>();
 
-         int m = rooms.length;
-         int n = rooms[0].length;
-
-         Queue<int[]> BFS = new ArrayDeque();
-         boolean[][] visited = new boolean[m][n];
-         int[][] directions = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-
-         for ( int i=0; i<m; i++ ) {
-            for ( int j=0; j<n; j++ ) {
+        for ( int i=0; i<n; i++ ) {
+            for ( int j=0; j<m; j++ ) {
                 if ( rooms[i][j] == 0 ) {
-                    BFS.add(new int[]{i, j});
+                    bfs.add(new int[]{i,j});
                     visited[i][j] = true;
                 }
             }
-         }
+        }
 
-         int levelSize = 0, levelCount = 0;
+        int level = 0;
 
-         while ( !BFS.isEmpty() ) {
-            levelSize = BFS.size();
-            while ( levelSize != 0 ) {
-                levelSize--;
-                int[] node = BFS.poll();
-                for ( int[] dir: directions ) {
-                    int dx = node[0] + dir[0];
-                    int dy = node[1] + dir[1];
-
-                    if ( dx<0 || dx>=m || dy<0 || dy>=n || visited[dx][dy] || rooms[dx][dy] != 2147483647 ) continue;
-                    rooms[dx][dy] = (levelCount+1);
+        while( !bfs.isEmpty() ) {
+            int size = bfs.size();
+            for ( int i=0; i<size; i++ ) {
+                int[] top = bfs.poll();
+                rooms[top[0]][top[1]] = level;
+                for ( int[] dir : directions ) {
+                    int dx = top[0] + dir[0];
+                    int dy = top[1] + dir[1];
+                    if ( dx<0 || dy<0 || dx>=n || dy>=m || rooms[dx][dy] == 0 || rooms[dx][dy] == -1 || visited[dx][dy] ) continue;
                     visited[dx][dy] = true;
-                    BFS.add(new int[]{dx, dy});
+                    bfs.add(new int[]{dx, dy});
                 }
             }
-            levelCount++;
-         }
-
+            level++;
+        }
 
     }
 }
