fab7db6f6b7c378c6dfdc8f7eb5201ce973c6111
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-07-14 18:26:18 -0700

    Time: 1 ms (46.23%), Space: 44.9 MB (84.08%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,44 +1,21 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // we have info
-        // minimum one value will exist
-        // matrix not null
-
-        // BFS
-        // [0, 0] => [-1, 0], [1, 0], [0, -1], [0, 1]
-        int[][] adj = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-
-        int m = image.length;
-        int n = image[0].length;
-
-        int source = image[sr][sc];
-
-        boolean[][] visited = new boolean[m][n];
-        // for ( int i=0; i<m; i++ ) {
-        //     for ( int j=0; j<n; j++ ) {
-        //         visited[i][j] = false;
-        //     }
-        // }
-
-        Queue<int[]> BFS = new LinkedList();
-        BFS.add(new int[]{sr, sc});
-
-        while ( !BFS.isEmpty() ) {
-            int[] node = BFS.poll();
-            int x = node[0], y = node[1];
-            if ( x < 0 || y < 0 || x >= m || y >= n || visited[x][y] || image[x][y] != source ) continue;
-
-            visited[x][y] = true;
-            image[x][y] = color;
-
-            for ( int i=0; i<4; i++ ) {
-                int newX = x + adj[i][0];
-                int newY = y + adj[i][1];
-                BFS.add( new int[]{newX, newY} );
-            }
+    private void dfs(int[][] image, boolean[][] visited, int[][] directions, int x, int y, int color) {
+        if ( visited[x][y] ) return;
+        visited[x][y] = true;
+        int cellValue = image[x][y];
+        image[x][y] = color;
+        for ( int[] dir : directions ) {
+            int dx = x + dir[0];
+            int dy = y + dir[1];
+            if ( dx<0 || dy<0 || dx>=image.length || dy>=image[0].length || image[dx][dy] != cellValue ) continue;
+            dfs(image, visited, directions, dx, dy, color);
         }
-
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n = image.length, m = image[0].length;
+        boolean[][] visited = new boolean[n][m];
+        int[][] directions = {{-1,0},{1,0},{0,1},{0,-1}};
+        dfs(image, visited, directions, sr, sc, color);
         return image;
-
     }
 }
