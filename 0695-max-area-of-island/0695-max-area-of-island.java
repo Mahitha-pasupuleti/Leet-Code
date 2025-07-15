8bd9649a05f6e5a6a924561c9c9e7400856117c4
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-07-15 12:04:08 -0700

    Time: 2 ms (68.88%), Space: 45 MB (33.56%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,30 +1,29 @@
 class Solution {
-    public int DFS(int[][] grid, boolean[][] visited, int[][] directions, int srX, int srY) {
-        visited[srX][srY] = true;
-        int area = 1;
+    private int dfs(int[][] grid, boolean[][] visited, int[][] directions, int x, int y) {
+        if ( visited[x][y] ) return 0;
+        visited[x][y] = true;
+        int minIslands = 1; // everytime he visits an island, we count it
         for ( int[] dir : directions ) {
-            int dx = srX + dir[0], dy = srY + dir[1];
-            if ( dx<0 || dx>=grid.length || dy<0 || dy>=grid[0].length || visited[dx][dy] || grid[dx][dy] != 1 ) continue;
-            area += DFS(grid, visited, directions, dx, dy);
+            int dx = x + dir[0];
+            int dy = y + dir[1];
+            if ( dx<0 || dy<0 || dx>=grid.length || dy>=grid[0].length || grid[dx][dy] == 0 ) continue;
+            minIslands += dfs(grid, visited, directions, dx, dy); // so no.of islands visited from given island are returned in the dfs
         }
-
-        return area;
+        return minIslands;
     }
     public int maxAreaOfIsland(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int[][] directions = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-
-        int max = 0;
-        for ( int i=0; i<m; i++ ) {
-            for ( int j=0; j<n; j++ ) {
-                if ( !visited[i][j] && grid[i][j] == 1 ) {
-                    max = Math.max(max, DFS(grid, visited, directions, i, j) );
+        int n = grid.length, m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
+        int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+        int maxArea = 0;
+        for (int i=0; i<n; i++ ) {
+            for ( int j=0; j<m; j++ ) {
+                if ( grid[i][j] == 1 && !visited[i][j] ) {
+                    int area = dfs(grid, visited, directions, i, j);
+                    maxArea = Math.max(maxArea, area);
                 }
             }
         }
-
-        return max;
+        return maxArea;
     }
 }
