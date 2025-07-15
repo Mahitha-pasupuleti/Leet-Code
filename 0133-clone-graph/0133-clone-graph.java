a17f950012e6ab8260a43c3b640e88dfd6fedfbd
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-07-15 13:28:22 -0700

    Time: 26 ms (49.78%), Space: 42.8 MB (54.02%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    private void dfs(Map<Integer, Node> map, Node orgHead, Node source) {
+        source.val = orgHead.val;
+        List<Node> neighborList = orgHead.neighbors;
+        map.put(orgHead.val, source);
+        if ( neighborList != null ) {
+            for ( Node neighbor : neighborList ) {
+                Node current = new Node(); 
+                if ( map.containsKey(neighbor.val) ) {
+                    current = map.get(neighbor.val);
+                }
+                source.neighbors.add( current ); 
+                if ( map.containsKey(neighbor.val) ) continue;
+                dfs(map, neighbor, current);
+            }
+        }
+    }
+    public Node cloneGraph(Node node) {
+        if ( node == null ) return null;
+        Node orgHead = node;
+        Node source = new Node();
+        Map<Integer, Node> map = new HashMap<>();
+
+        dfs(map, orgHead, source); // 1, 0
+        return source;
+    }
+}
