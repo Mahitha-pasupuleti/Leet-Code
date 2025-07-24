60d21ba9b3cfe60d1bfbe25ce6bff2cec01bde23
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-07-23 17:42:09 -0700

    Time: 2 ms (76.31%), Space: 44.9 MB (49.93%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,36 +1,52 @@
-class Solution {
-    public void DFS(Map<Integer, List<Integer>> adjList, boolean[] visited, int source) {
-        visited[source] = true;
-        List<Integer> neighbours = adjList.get(source);
-        if ( neighbours != null ) {
-            for ( int neighbour : neighbours ) {
-                if ( !visited[neighbour] ) {
-                    DFS(adjList, visited, neighbour);
-                }
+class DSU {
+    private int[] rank;
+    private int[] parent;
+
+    public DSU(int n) {
+        rank = new int[n];
+        parent = new int[n];
+        for ( int i=0; i<n; i++ ) {
+            rank[i] = 1;
+            parent[i] = i;
+        }
+    }
+
+    public int find(int node) {
+        if ( parent[node] == node ) return node;
+        // path compression
+        return parent[node] = find( parent[node] );
+    }
+
+    public void union(int a, int b) {
+        int leader_a = find(a);
+        int leader_b = find(b);
+
+        if ( leader_a != leader_b ) {
+            if ( rank[leader_a] < rank[leader_b] ) {
+                rank[leader_b] += rank[leader_a];
+                parent[leader_a] = leader_b;
+            } else {
+                rank[leader_a] += rank[leader_b];
+                parent[leader_b] = leader_a;
             }
         }
     }
+
+    public int getComponentCount() {
+        Set<Integer> uniqueLeaders = new HashSet<>();
+        for (int i = 0; i < parent.length; i++) {
+            uniqueLeaders.add(find(i));  // find ensures path compression
+        }
+        return uniqueLeaders.size();
+    }
+
+}
+class Solution {
     public int countComponents(int n, int[][] edges) {
-        
-        // Adjacency List
-        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        DSU disjoin = new DSU(n);
         for ( int[] edge : edges ) {
-            adjList.computeIfAbsent(edge[0], k -> new ArrayList()).add(edge[1]);
-            adjList.computeIfAbsent(edge[1], k -> new ArrayList()).add(edge[0]);
+            disjoin.union(edge[0], edge[1]);
         }
-
-        // Visited
-        boolean[] visited = new boolean[n];
-
-        int connectedComponents = 0;
-
-        for ( int i=0; i<n; i++ ) {
-            if ( !visited[i] ) {
-                connectedComponents++;
-                DFS(adjList, visited, i);
-            }
-        }
-
-        return connectedComponents;
+        return disjoin.getComponentCount();
     }
 }
