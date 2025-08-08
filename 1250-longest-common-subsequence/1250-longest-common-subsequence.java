8c21eb494fe5069d5fca33dc0e2c367d8296638f
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-08-08 11:55:30 -0700

    Time: 19 ms (86.3%), Space: 51.3 MB (13.29%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,25 +1,8 @@
 class Solution {
-    // Good naming convention
-    private int findLongestCommonSubsequence(String text1, String text2, int n, int m, int[][] dp) {
-        if ( n == 0 || m == 0 ) return 0;
-        if ( dp[n][m] != -1 ) return dp[n][m];
-        if ( text1.charAt(n-1) == text2.charAt(m-1) ) {
-            return dp[n][m] = 1 + findLongestCommonSubsequence(text1, text2, n-1, m-1, dp);
-        }
-        return dp[n][m] = Math.max( findLongestCommonSubsequence(text1, text2, n-1, m, dp), findLongestCommonSubsequence(text1, text2, n, m-1, dp) );
-    }
     public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
-        int[][] dp = new int[n + 1][m + 1];
-        // for ( int i=0; i<=text1_Len; i++ ) {
-        //     Arrays.fill(dp[i], -1);
-        // }
-        // return findLongestCommonSubsequence(text1, text2, n, m, dp);
+        int n = text1.length(), m = text2.length();
+        int[][] dp = new int[n+1][m+1];
 
-        // if text1 is empty string then, for any length of text2, the result is 1
-        //    "" a b c
-        // ""  1.1 1 1
         for ( int i=0; i<=n; i++ ) {
             dp[i][0] = 0;
         }
@@ -38,6 +21,34 @@ class Solution {
             }
         }
 
+        // for ( int i=1; i<=n; i++ ) {
+        //     for ( int j=1; j<=m; j++ ) {
+        //         System.out.print( dp[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int i = n, j = m, len = dp[n][m];
+        // StringBuilder result = new StringBuilder();
+        // while ( len != 0 ) {
+        //     if ( text1.charAt(i-1) == text2.charAt(j-1) ) {
+        //         result.append(text1.charAt(i-1));
+        //         len--;
+        //         i--;
+        //         j--;
+        //     } else {
+        //         if ( dp[i][j-1] > dp[i-1][j] ) {
+        //             j--;
+        //         } else {
+        //             i--;
+        //         }
+        //     }
+        // }
+
+        // System.out.println( result.toString() );
+
         return dp[n][m];
+
+        // return result.reverse().toString();
     }
 }
