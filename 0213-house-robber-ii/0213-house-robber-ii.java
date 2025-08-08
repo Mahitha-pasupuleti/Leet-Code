ab11e88f4372ba4a6ee8274a8d76c16998e7e9a9
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-08-08 09:47:12 -0700

    Time: 0 ms (100%), Space: 40.9 MB (91.17%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,20 +1,22 @@
 class Solution {
     int[] dp;
-    public int solve(int[] nums, int i, int j) {
-        if ( i >= j ) return 0;
-        if ( dp[j] != -1 ) return dp[j];
-        int pick = nums[j-1] + solve(nums, i, j-2); // if taken no adjacent
-        int notPick = solve(nums, i, j-1); // else continue adjacent
-        return dp[j] = Math.max(pick, notPick);
+    private int solve(int start, int end, int[] nums ) {
+        if ( start >= end ) return 0;
+        if ( dp[start] != -1 ) return dp[start];
+        int pick = nums[start] + solve(start+2, end, nums); // picked first house
+        int notPick = solve(start+1, end, nums); // not picked first house
+        return dp[start] = Math.max( pick, notPick );
     }
     public int rob(int[] nums) {
         int n = nums.length;
         if ( n == 1 ) return nums[0];
         dp = new int[n+1];
+
         Arrays.fill(dp, -1);
-        int first = solve(nums, 0, n-1); // with 0, you dont want to involve last value since they are adjacent
+        int withZero = solve(0, n-1, nums);
+
         Arrays.fill(dp, -1);
-        int second = solve(nums, 1, n);
-        return Math.max(first, second);
+        int withoutZero = solve(1, n, nums);
+        return Math.max( withZero, withoutZero );
     }
 }
