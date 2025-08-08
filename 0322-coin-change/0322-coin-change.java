2208d6a5e8080fc0c852939ec28b7e6ed1d4f53a
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-08-08 15:41:18 -0700

    Time: 36 ms (12.98%), Space: 45.5 MB (14.56%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,25 @@
 class Solution {
+    int[][] dp;
+    private int solve(int start, int[] coins, int amount) {
+        if ( start >= coins.length || amount<0 ) return Integer.MAX_VALUE;
+        if ( dp[start][amount] != -1 ) return dp[start][amount];
+        if ( amount == 0 ) return 0;
+
+        int pick = solve(start, coins, amount - coins[start]);
+        if ( pick != Integer.MAX_VALUE ) pick = 1 + pick; // ******u need to add 1 only when u pick something*******
+
+        int notPick = solve(start+1, coins, amount);
+        int result = Math.min(pick, notPick);
+
+        return dp[start][amount] = Math.min(pick, notPick);
+    }
     public int coinChange(int[] coins, int amount) {
-        // TOP-DOWN APPROACH
-        int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
-
-        for ( int j=0; j<amount+1; j++ ) dp[0][j] = Integer.MAX_VALUE - 1; // when n=0, for any amount
-        for ( int i=1; i<n+1; i++ ) dp[i][0] = 0; // when amount=0, for any number of coins
-
-        for ( int j=1; j<amount+1; j++ ) {
-            if ( j % coins[0] == 0 ) dp[1][j] = j / coins[0];
-            else dp[1][j] = Integer.MAX_VALUE - 1;
+        dp = new int[coins.length+1][amount+1];
+        for ( int i=0; i<coins.length; i++ ) {
+            Arrays.fill(dp[i], -1);
         }
-    
-        for ( int i=2; i<n+1; i++ ) {
-            for ( int j=1; j<amount+1; j++ ) {
-                if ( coins[i-1] <= j ) {
-                    dp[i][j] = Math.min( 1 + dp[i][j-coins[i-1]] , dp[i-1][j] );
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-
-        return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
+        int result = solve(0, coins, amount);
+        if ( result == Integer.MAX_VALUE ) return -1;
+        return result;
     }
 }
