9193dde53645b30cca2df6257a6ac37ba4485b65
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-08-07 17:17:32 -0700

    Time: 0 ms (100%), Space: 40.2 MB (92.04%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,16 +1,14 @@
 class Solution {
     int[] dp;
-    public int solve(int n) { // 4 // 3
-    if ( dp[n] != -1 ) return dp[n];
-        if ( n == 0 ) return 0;
+    public int solve(int n) {
+        if ( dp[n] != -1 ) return dp[n];
+        if ( n <= 0 ) return 0;
         if ( n == 1 || n == 2 ) return 1;
-        return dp[n] = solve(n-1) + solve(n-2) + solve(n-3); // 3 + 2 + 1 // 2 + 1 + 0
+        return dp[n] = solve(n-1) + solve(n-2) + solve(n-3);
     }
     public int tribonacci(int n) {
         dp = new int[n+1];
-        for ( int i=0; i<n+1; i++ ) {
-            dp[i] = -1;
-        }
+        Arrays.fill(dp, -1);
         return solve(n);
     }
 }
