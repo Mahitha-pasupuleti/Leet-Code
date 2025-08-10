2d557b83fad28fdc47db450c4ab43412ccc9a9a7
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-08-10 11:05:11 -0700

    Time: 21 ms (92.67%), Space: 57.5 MB (25.88%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,39 +1,29 @@
 class Solution {
+    // find a subarray with sum/2
+    // corner cases
+    // [1]
     Boolean[][] dp;
-    public boolean subsetSum(int[] nums, int sum, int n) {
-        if ( sum == 0 ) return true;
-        if ( n == 0 ) return false;
+    private boolean solve(int start, int[] nums, int amount) {
+        if ( amount == 0 ) return true;
+        if ( start == nums.length ) return false;
 
-        if (dp[n][sum] != null) return dp[n][sum];
+        if ( dp[start][amount] != null ) return dp[start][amount];
 
-        if ( nums[n-1] <= sum ) {
-            return dp[n][sum] = subsetSum(nums, sum-nums[n-1], n-1) || subsetSum( nums, sum, n-1);
-        } 
-        else return dp[n][sum] = subsetSum(nums, sum, n-1);
+        if ( nums[start] <= amount ) {
+            return dp[start][amount] = solve(start+1, nums, amount-nums[start]) || solve(start+1, nums, amount);
+        } else {
+            return dp[start][amount] = solve(start+1, nums, amount);
+        }
     }
     public boolean canPartition(int[] nums) {
-        // if two distinct subsets have same sum, return true;
-        // max can be sum/2
-        // if we are able to get a subset with sum/2, then automatically the remaining value will be sum/2
-        /*
-         To explain clearly
-         - [1, 5, 11, 5]
-         - total sum = 22
-         - if we are able to find a subset whose indiviualSum = totoalSum/2
-         - then the other subset individualSum will be automatically totalSum/2 as well
-        */
+        int sum = 0;
+        for ( int num : nums ) sum += num;
+        // if sum is odd in first place, then there is no partion present
+        if ( sum % 2 != 0 ) return false;
 
-        int targetSum = 0;
-        for ( int num: nums ) {
-            targetSum += num;
-        }
+        dp = new Boolean[nums.length + 1][sum/2 + 1];
 
-        if ( targetSum % 2 != 0 ) return false;
-        
-        targetSum /= 2;
-
-        dp = new Boolean[nums.length + 1][targetSum + 1];
-
-        return subsetSum(nums, targetSum, nums.length);
+        // find subarray with given sum
+        return solve(0, nums, sum/2);
     }
 }
