cf9ee4872100fc8b3cb78706a6d459b328a90fe8
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-08-10 16:20:19 -0700

    Time: 5 ms (48%), Space: 45 MB (29.07%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -2,27 +2,28 @@ class Solution {
     public int deleteAndEarn(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        int[] dp = new int[n + 1]; // dp[n] = 0 by default
+        int[] dp = new int[n + 1]; // dp[n] = 0
 
-        for (int start = n - 1; start >= 0; start--) {
-            int i = start + 1;
-            int val = nums[start];
-            int earnedPoints = nums[start];
+        for (int i = n - 1; i >= 0; i--) {
+            int nextIndex = i + 1;
+            int val = nums[i];
+            int earnedPoints = val;
 
             // collect duplicates
-            while (i < n && nums[i] == val) {
+            while (nextIndex < n && nums[nextIndex] == val) {
                 earnedPoints += val;
-                i++;
+                nextIndex++;
             }
 
-            // skip all nums == val + 1
-            while (i < n && nums[i] - val == 1) {
-                i++;
+            // skip adjacent numbers
+            while (nextIndex < n && nums[nextIndex] - val == 1) {
+                nextIndex++;
             }
 
-            int take = earnedPoints + dp[i];
-            int notTake = dp[start + 1];
-            dp[start] = Math.max(take, notTake);
+            int take = earnedPoints + dp[nextIndex];
+            int notTake = dp[i + 1];
+
+            dp[i] = Math.max(take, notTake);
         }
 
         return dp[0];
