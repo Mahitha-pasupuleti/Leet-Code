c39d1ad46e58d4d3d1e2ac9b38f9acc06fb61e17
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-08-10 16:13:53 -0700

    Time: 6 ms (42.61%), Space: 44.8 MB (36.33%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,33 +1,30 @@
 class Solution {
-    int[] dp;
-    private int solve(int start, int[] nums) {
-        if ( start >= nums.length ) return 0;
-        if ( dp[start] != -1 ) return dp[start];
-
-        // if adjacent equal add points and skip them;
-        int i = start+1;
-        int val = nums[start];
-        int earnedPoints = nums[start];
-        
-        while ( i < nums.length && nums[i] == val ) {
-            earnedPoints += val;
-            i++;
-        }
-
-        while ( i < nums.length && nums[i]-val == 1 ) {
-            i++;
-        }
-
-        int take = earnedPoints + solve(i, nums);
-        int notTake = solve(start+1, nums);
-
-        return dp[start] = Math.max(take, notTake);
-    }
     public int deleteAndEarn(int[] nums) {
         Arrays.sort(nums);
-        dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        
-        return solve(0, nums);
+        int n = nums.length;
+        int[] dp = new int[n + 1]; // dp[n] = 0 by default
+
+        for (int start = n - 1; start >= 0; start--) {
+            int i = start + 1;
+            int val = nums[start];
+            int earnedPoints = nums[start];
+
+            // collect duplicates
+            while (i < n && nums[i] == val) {
+                earnedPoints += val;
+                i++;
+            }
+
+            // skip all nums == val + 1
+            while (i < n && nums[i] - val == 1) {
+                i++;
+            }
+
+            int take = earnedPoints + dp[i];
+            int notTake = dp[start + 1];
+            dp[start] = Math.max(take, notTake);
+        }
+
+        return dp[0];
     }
 }
