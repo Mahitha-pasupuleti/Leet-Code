a2252b7a40d860f6976b5b93f6fd76b79409d3fd
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-08-10 13:00:09 -0700

    Time: 3 ms (64.01%), Space: 45.5 MB (25.36%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,40 +1,18 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[n][n];
-        
-        for ( int j=0; j<n; j++ ) {
-            dp[n-1][j] = triangle.get(n-1).get(j);
+        int[] dp = new int[n];
+
+        for ( int i=0; i<n; i++ ) {
+            dp[i] = triangle.get(n-1).get(i);
         }
 
-        for ( int i=n-2; i>=0; i-- ) { // rows from last
+        for ( int i=n-2; i>=0; i-- ) {
             for ( int j=0; j<=i; j++ ) {
-                dp[i][j] = triangle.get(i).get(j) + Math.min( dp[i+1][j], dp[i+1][j+1] );
+                dp[j] = triangle.get(i).get(j) + Math.min( dp[j], dp[j+1] );
             }
         }
 
-        return dp[0][0];
+        return dp[0];
     }
 }
-
-/*
-class Solution {
-    public int solve( int start, int end, int n, List<List<Integer>> triangle, int[][] dp ) {
-        if ( start >= n || end >= n ) return Integer.MAX_VALUE;
-        if ( dp[start][end] != -1 ) return dp[start][end];
-        if ( start == n-1 ) return 0;
-        return dp[start][end] = Math.min( 
-            triangle.get(start+1).get(end) + solve(start+1, end, n, triangle, dp), 
-            triangle.get(start+1).get(end+1) + solve(start+1, end+1, n, triangle, dp) 
-        );
-    }
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[][] dp = new int[n][n];
-        for ( int i=0; i<n; i++ ) {
-            Arrays.fill(dp[i], -1);
-        }
-        return triangle.get(0).get(0) + solve(0, 0, n, triangle, dp);
-    }
-}
-*/
