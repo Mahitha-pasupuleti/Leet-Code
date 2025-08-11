a3bdf557463fa46801c20c0c184133c4358170c5
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-08-11 14:30:37 -0700

    Time: 0 ms (100%), Space: 41.4 MB (86.12%) - LeetHub

---
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -0,0 +1,31 @@
+class Solution {
+    int[] dp;
+    private int solve(int start, int[] days, int[] costs) {
+        if ( start >= days.length ) return 0;
+        if ( dp[start] != -1 ) return dp[start];
+        // day pass
+        int dayPass = costs[0] + solve(start+1, days, costs);
+        // week pass
+        int weekPass = costs[1];
+        for ( int i=start+1; i<days.length; i++ ) {
+            if ( days[i] > days[start]+6 ) {
+                //System.out.println( days[i] );
+                weekPass += solve(i, days, costs);
+                break;
+            }
+        }
+        int monthPass = costs[2];
+        for ( int i=start+1; i<days.length; i++ ) {
+            if ( days[i] > days[start]+29 ) {
+                monthPass += solve(i, days, costs);
+                break;
+            }
+        }
+        return dp[start] = Math.min(dayPass, Math.min(weekPass, monthPass));
+    }
+    public int mincostTickets(int[] days, int[] costs) {
+        dp = new int[days.length+1];
+        Arrays.fill(dp, -1);
+        return solve(0, days, costs);
+    }
+}
