67b2e044c44bd439272d811e1a9bdd57777235b4
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-08-12 12:21:34 -0700

    Time: 95 ms (22.42%), Space: 43.4 MB (31.24%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,48 +1,24 @@
 class Solution {
-    int[][] dp;
-    public int solve(int[] squares, int i, int target) {
-        // till target becomes 0
-        if ( i < 0 ) return Integer.MAX_VALUE; // some maximum value
-        if ( dp[i][target] != -1 ) return dp[i][target];
-        if ( target == 0 ) return 0;
+    int[] dp;
+    private int solve(int n) {
+        if ( n == 0 ) return 0;
+        if ( n < 0 ) return Integer.MAX_VALUE;
+        int root = (int)Math.sqrt(n);
+        if ( dp[n] != -1 ) return dp[n];
 
-        int take = Integer.MAX_VALUE;
-        if ( squares[i] <= target ) {
-            int result = solve(squares, i, target-squares[i]); // TAKE: the outcome could be 0 or MAX_VALUE, because those are only values being returned
-            if ( result != Integer.MAX_VALUE ) take = 1 + result; // got a combination whose sum == target
-        }
-        
-        int noTake = solve(squares, i-1, target); //NOTAKE: the outcome could be 0 or MAX_VALUE, because those are only values being returned
-        return dp[i][target] = Math.min(take, noTake);
-    }
-    public int numSquares(int n) {
-        // Create new array with all perfect sqaure
-        int noOfPerfectSqaures = 0;
-        for ( int i=1; i*i <= n; i++ ) {
-            // System.out.println(noOfPerfectSqaures+1);
-            noOfPerfectSqaures++;
-        }
-
-        int[] squares = new int[noOfPerfectSqaures];
-        int index = 0;
-        for ( int i=1; i*i <= n; i++ ) {
-            squares[index++] = i*i;
-        }
-
-        dp = new int[noOfPerfectSqaures+1][n+1];
-        for ( int i=0; i<noOfPerfectSqaures; i++ ) {
-            for ( int j=0; j<n+1; j++ ) {
-                dp[i][j] = -1;
+        int minCount = Integer.MAX_VALUE;
+        for ( int i=1; i*i<=n; i++ ) {
+            int result = solve(n - i*i);
+            if ( result != Integer.MAX_VALUE ) {
+                minCount = Math.min(minCount, 1+result);
             }
         }
-
-        return solve(squares, noOfPerfectSqaures-1, n); // n-1
+        
+        return dp[n] = minCount;
+    }
+    public int numSquares(int n) {
+        dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return solve(n);
     }
 }
-
-/*
-
-12 : [1, 4, 9] : find least length sum combination of given array
- - 
-
-*/
