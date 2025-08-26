0abb8c876df71a450fa676e02929f195de06ba68
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-08-26 16:47:51 -0700

    Time: 1 ms (99.52%), Space: 57.5 MB (8.25%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,65 +1,15 @@
 class Solution {
-    /*
-    Optimal approach - Kadane's Algorithm
-    -> O(n) - TC
-    -> O(1) - SC
-    */
-
     public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
         int maxSum = nums[0];
-        int sum = nums[0];
+
         for ( int i=1; i<nums.length; i++ ) {
-            sum = Math.max( sum + nums[i], nums[i] );
-            maxSum = Math.max( sum, maxSum );
+            // Extend the subarray with nums[i]
+            // Start new subarray with nums[i]
+            currentSum = Math.max( nums[i], currentSum + nums[i] );
+            maxSum = Math.max( maxSum, currentSum );
         }
-        // if ( maxSum < 0 ) return 0;
+
         return maxSum;
     }
 }
-
-/*
- - Brute force
- - Better Approach
- - Kadane's algorithm
-*/
-
-    /* 
-    Brute force approach 
-    -> O(n^3) - TC
-    -> O(1) - SC
-    
-    public int maxSubArray(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;;
-        for ( int i=0; i<nums.length; i++ ) {
-            for ( int j=i; j<nums.length; j++ ) {
-                int sum = 0;
-                for ( int k=i; k<=j; k++ ) {
-                    sum += nums[k];
-                }
-                maxSum = Math.max(sum, maxSum);
-            }
-        }
-
-        return maxSum;
-    } 
-
-    */
-
-    /*
-    Better approach 
-    -> O(n^2) - TC
-    -> O(1) - SC
-    
-    public int maxSubArray(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;
-        for ( int i=0; i<nums.length; i++ ) {
-            int sum = 0;
-            for ( int j=i; j<nums.length; j++ ) {
-                sum += nums[j];
-                maxSum = Math.max(sum, maxSum);
-            }
-        }
-
-        return maxSum;
-    }
-    */
