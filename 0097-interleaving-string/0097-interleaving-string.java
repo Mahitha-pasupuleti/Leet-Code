b6993d945a61a9ae14529557b45c6a986fa605fc
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-09-02 16:51:58 -0700

    Time: 1 ms (87.83%), Space: 42 MB (44.49%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,9 +1,8 @@
 class Solution {
-    Map<String, Boolean> memo;
+    Boolean[][] dp;
     private boolean solve(int i, int j, int k, String s1, String s2, String s3) {
         if ( k == s3.length() ) return i==s1.length() && j==s2.length();
-        String key = i + ":" + j + ":" + k;
-        if ( memo.containsKey(key) ) return memo.get(key);
+        if ( dp[i][j] != null ) return dp[i][j];
         boolean s1Take = false;
         boolean s2Take = false;
         if ( i<s1.length() && k<s3.length() && s1.charAt(i) == s3.charAt(k) ) {
@@ -12,13 +11,11 @@ class Solution {
         if ( j<s2.length() && k<s3.length() && s2.charAt(j) == s3.charAt(k) ) {
             s2Take = solve(i, j+1, k+1, s1, s2, s3);
         }
-        boolean value = s1Take || s2Take;
-        memo.put(key, value);
-        return value;
+        return dp[i][j] = s1Take || s2Take;
     }
     public boolean isInterleave(String s1, String s2, String s3) {
         if ( s1.length() + s2.length() != s3.length() ) return false;
-        memo = new HashMap<>();
+        dp = new Boolean[s1.length()+1][s2.length()+1];
         return solve(0, 0, 0, s1, s2, s3);
     }
 }
