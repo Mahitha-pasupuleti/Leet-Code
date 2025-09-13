8d1d2bf4050be49fcfe98bbe93d674724cd72910
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-09-13 13:34:50 -0700

    Time: 23 ms (5.71%), Space: 47.8 MB (6.57%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,19 +1,21 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort( intervals, (a, b) -> (a[0] - b[0]) );
-
-        int noofMeetingRooms = 0;
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // Min-Heap
-
-        for ( int i=0; i<intervals.length; i++ ) {
-            if ( !pq.isEmpty() && pq.peek() <= intervals[i][0] ) 
-                pq.poll(); // if not overlapping free up prior 
-            else
-                noofMeetingRooms++;
-            pq.add(intervals[i][1]);
+        Map<Integer, Integer> map = new TreeMap<>();
+        for ( int[] interval : intervals ) {
+            map.put(interval[0], map.getOrDefault(interval[0], 0) + 1);
+            map.put(interval[1], map.getOrDefault(interval[1], 0) - 1);
         }
+        System.out.println( map );
 
-        return noofMeetingRooms;
+        int maxRooms = 0;
+        int currentRooms = 0;
+        for ( int value : map.values() ) {
+            currentRooms += value;
+            maxRooms = Math.max(maxRooms, currentRooms );
+        }
+        return maxRooms;
     }
 }
+
+// {0=1, 5=1, 15=-1, 16=1, 20=-1, 30=-1} the max will remain 2 since, one meeting is ending before starting another.
+// This si genuius alogorithm
