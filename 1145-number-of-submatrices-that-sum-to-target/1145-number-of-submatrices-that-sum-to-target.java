56161a138c42110a016dbe689a23853e02aa125a
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-09-20 16:54:47 -0700

    Time: 146 ms (35.82%), Space: 46.1 MB (5.67%) - LeetHub

---
+++ 1145-number-of-submatrices-that-sum-to-target/1145-number-of-submatrices-that-sum-to-target.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        // Build Prefix Sum
+        int m = matrix.length, n = matrix[0].length;
+        int[][] prefix = new int[m+1][n+1];
+
+        for ( int row = 1; row<=m; row++ ) {
+            for ( int col=1; col<=n; col++ ) {
+                prefix[row][col] = matrix[row-1][col-1] + prefix[row-1][col] + prefix[row][col-1] - prefix[row-1][col-1];
+            }
+        }
+
+        int count = 0;
+
+        // get how many submatrices with sum = target
+        for ( int row1=1; row1<=m; row1++ ) {
+            for ( int row2=row1; row2<=m; row2++ ) {
+                Map<Integer, Integer> map = new HashMap<>();
+                map.put(0, 1);
+                int currSum = 0;
+                for ( int col=1; col<=n; col++ ) {
+                    currSum = prefix[row2][col] - prefix[row1-1][col];
+                    int value = currSum - target;
+                    if ( map.containsKey(value) ) count += map.get(value);
+                    map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+                }
+            }
+        }
+
+        return count;
+    }
+}
