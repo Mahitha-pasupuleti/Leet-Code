8d9217b73c1fe53f3853363103956ff286903a4c
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-09-20 17:15:02 -0700

    Time: 137 ms (61.34%), Space: 44.1 MB (98.45%) - LeetHub

--- 1145-number-of-submatrices-that-sum-to-target/1145-number-of-submatrices-that-sum-to-target.java
+++ 1145-number-of-submatrices-that-sum-to-target/1145-number-of-submatrices-that-sum-to-target.java
@@ -12,23 +12,39 @@ class Solution {
 
         int count = 0;
 
-        // get how many submatrices with sum = target
         for ( int row1=1; row1<=m; row1++ ) {
             for ( int row2=row1; row2<=m; row2++ ) {
-                Map<Integer, Integer> map = new HashMap<>();
-                map.put(0, 1);
-                int currSum = 0;
-                for ( int col=1; col<=n; col++ ) {
-                    int colSum = prefix[row2][col] - prefix[row1 - 1][col] 
-                               - prefix[row2][col - 1] + prefix[row1 - 1][col - 1];
-
-                    currSum += colSum;
-                    int value = currSum - target;
-                    if ( map.containsKey(value) ) count += map.get(value);
-                    map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+                for ( int col1=1; col1<=n; col1++ ) {
+                    for ( int col2=col1; col2<=n; col2++ ) {
+                        int prefixSum = prefix[row2][col2] - prefix[row1-1][col2] - prefix[row2][col1-1] + prefix[row1-1][col1-1];
+                        if ( prefixSum == target ) count++;
+                    }
                 }
             }
         }
+
+        // int count = 0;
+
+        // // get how many submatrices with sum = target
+        // for ( int row1=1; row1<=m; row1++ ) {
+        //     for ( int row2=row1; row2<=m; row2++ ) {
+        //         Map<Integer, Integer> map = new HashMap<>();
+        //         map.put(0, 1);
+        //         int currSum = 0;
+        //         for ( int col=1; col<=n; col++ ) {
+        //             currSum = prefix[row2][col] - prefix[row1-1][col];
+        //             /*
+        //             int colSum = prefix[row2][col] - prefix[row1 - 1][col] 
+        //                        - prefix[row2][col - 1] + prefix[row1 - 1][col - 1];
+
+        //             currSum += colSum;
+        //             */
+        //             int value = currSum - target;
+        //             if ( map.containsKey(value) ) count += map.get(value);
+        //             map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+        //         }
+        //     }
+        // }
 
         return count;
     }
