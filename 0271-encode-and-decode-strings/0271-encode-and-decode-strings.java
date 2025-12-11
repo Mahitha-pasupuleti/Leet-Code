9aff8329d24af68913209f355eab56eb8aa172de
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-12-10 19:40:16 -0800

    Time: 7 ms (48.97%), Space: 47.1 MB (54.45%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -4,59 +4,23 @@ public class Codec {
     public String encode(List<String> strs) {
         StringBuilder result = new StringBuilder();
         for ( String str : strs ) {
-            result.append(str.length()).append('#').append(str);
+            result.append(str);
+            result.append("∞");
         }
-        System.out.println(result);
         return result.toString();
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        List<String> result = new ArrayList();
-        int i=0;
-        while ( i<s.length() ) {
-            int hashIndex = s.indexOf('#', i);
-            System.out.println(hashIndex);
-            int strLen = Integer.parseInt(s.substring(i, hashIndex));
-            System.out.println(strLen);
-            i = hashIndex + 1;
-            result.add( s.substring(i, i + strLen) );
-            i = i + strLen;
+        List<String> resultList = new ArrayList<>();
+        String[] decodeList = s.split("∞", -1);
+
+        for ( int i=0; i<decodeList.length-1; i++ ) {
+            resultList.add(decodeList[i]);
         }
-        return result;
+        return resultList;
     }
 }
-
-// public class Codec {
-
-//     // Encodes a list of strings to a single string.
-//     public String encode(List<String> strs) {
-//         StringBuilder result = new StringBuilder();
-//         for( int i=0; i<strs.size(); i++ ) {
-//             for ( int j=0; j<strs.get(i).length(); j++ ) {
-//                 result.append(strs.get(i).charAt(j));
-//             }
-//             result.append("क");
-//         }
-//         return result.toString();
-//     }
-
-//     // Decodes a single string to a list of strings.
-//     public List<String> decode(String s) {
-//         List<String> result = new ArrayList();
-//         StringBuilder curr = new StringBuilder();
-//         for ( int i=0; i<s.length(); i++ ) {
-//             if ( s.charAt(i) != 'क' ) {
-//                 curr.append(s.charAt(i));
-//             } else {
-//                 result.add(curr.toString());
-//                 curr = new StringBuilder();;
-//             }
-//         }
-
-//         return result;
-//     }
-// }
 
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
