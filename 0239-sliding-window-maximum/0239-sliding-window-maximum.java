4b9326f7d0b6b6b6bd080635448801e35cb6a83c
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-12-12 18:51:54 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,51 +1,20 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        int[] result = new int[n-k+1];
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] result = new int[nums.length-k+1];
         int index = 0;
 
-        Deque<Integer> queue = new ArrayDeque<>();
-        for ( int i=0; i<n; i++ ) {
-            if ( !queue.isEmpty() && queue.peek() == i-k ) queue.poll();
-            while ( !queue.isEmpty() && nums[queue.peekLast()] < nums[i] ) queue.pollLast();
-            queue.offer(i);
+        for ( int i=0; i<nums.length; i++ ) {
+            if ( !dq.isEmpty() && dq.peekFirst() == i-k ) dq.pollFirst();
+            while ( !dq.isEmpty() && nums[dq.peekLast()] >= nums[i] ) {
+                dq.pollLast();
+            }
+            dq.addLast(i);
             if ( i >= k-1 ) {
-                result[index++] = nums[queue.peek()];
+                result[index++] = nums[dq.peekFirst()];
             }
         }
 
         return result;
     }
 }
-
-/*
-class Solution {
-    public int getMax(int start, int end, int[] nums) {
-        int max = Integer.MIN_VALUE;
-        for ( int i=start; i<=end; i++ ) {
-            if ( nums[i] > max ) max = nums[i];
-        }
-        return max;
-    }
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int start=0, end=0;
-        List<Integer> result = new ArrayList();
-        // int[] finalResult = new int[2];
-
-        while ( end < nums.length ) {
-            if ( end - start + 1 == k ) {
-                result.add(getMax(start, end, nums));
-                start++;
-            }
-            end++;
-        }
-
-        // for ( int i=0; i<result.size(); i++ ) {
-        //     System.out.println(result.get(i));
-        // }
-
-        int[] finalResult = result.stream().mapToInt(Integer::intValue).toArray();
-        return finalResult;
-    }
-}
-*/
