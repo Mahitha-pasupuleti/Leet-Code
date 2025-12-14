3b759953ca6655219c557fe466e88d7ba0ad098a
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-12-14 02:11:24 -0800

    Time: 295 ms (42.58%), Space: 47.1 MB (47.99%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -2,20 +2,20 @@ class Node {
     Node[] links = new Node[26];
     boolean flag = false;
 
-    boolean contains(char ch) {
+    boolean containsKey(char ch) {
         return links[ch - 'a'] != null;
-    }
-
-    void add(char ch, Node node) {
-        links[ch - 'a'] = node;
     }
 
     Node get(char ch) {
         return links[ch - 'a'];
     }
 
-    void setEnd(boolean val) {
-        flag = val;
+    void put(char ch, Node node) {
+        links[ch - 'a'] = node;
+    }
+
+    void setEnd(boolean value) {
+        flag = value;
     }
 
     boolean isEnd() {
@@ -23,74 +23,65 @@ class Node {
     }
 }
 class Solution {
-    private Node root = new Node();
-    int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+    // BACKTRACKING TRIE
+    private Node root;
+    int[][] directions = {{-1,0},{1,0},{0,1},{0,-1}};
 
-    private void dfs(int x, int y, char[][] board, Node node, StringBuilder str, Set<String> result) {
-        char ch = board[x][y];
-        node = node.get(ch);
-        str.append(ch);
-        if (node.isEnd()) {
-            result.add(str.toString());
-            node.setEnd(false); // prune
-        }
-
-        board[x][y] = '#'; // mark visited
-        for (int[] dir : directions) {
-            int dx = x + dir[0], dy = y + dir[1];
-            if (dx < 0 || dy < 0 || dx >= board.length || dy >= board[0].length) continue;
-            if (board[dx][dy] == '#') continue;
-            if (!node.contains(board[dx][dy])) continue;
-            dfs(dx, dy, board, node, str, result);
-        }
-        board[x][y] = ch; // backtrack
-        str.deleteCharAt(str.length() - 1);
+    public Solution() {
+        root = new Node();
     }
 
     private void insert(String word) {
         Node node = root;
         for ( int i=0; i<word.length(); i++ ) {
             char ch = word.charAt(i);
-            if ( !node.contains(ch) ) {
-                node.add(ch, new Node());
+            if ( !node.containsKey(ch) ) {
+                node.put(ch, new Node());
             }
             node = node.get(ch);
         }
         node.setEnd(true);
     }
 
+    private void dfs(int x, int y, StringBuilder str, char[][] board, boolean[][] visited, Node node, List<String> result) {
+        visited[x][y] = true;
+        str.append(board[x][y]);
+        if ( node.isEnd() ) {
+            node.setEnd(false);
+            result.add(str.toString());
+        }
+        for ( int[] dir : directions ) {
+            int dx = x + dir[0];
+            int dy = y + dir[1];
+            if ( dx<0 || dy<0 || dx>=board.length || dy>=board[0].length || visited[dx][dy] ) continue;
+            if ( !node.containsKey( board[dx][dy] ) ) continue;
+            dfs(dx, dy, str, board, visited, node.get( board[dx][dy] ), result);
+        }
+        visited[x][y] = false;
+        str.deleteCharAt(str.length()-1);
+    }
+
     public List<String> findWords(char[][] board, String[] words) {
-        Node node = root;
         int m = board.length, n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        List<String> result = new ArrayList<>();
+        StringBuilder str = new StringBuilder();
 
         for ( String word : words ) {
             insert(word);
         }
 
-        Set<String> result = new HashSet<>();
-        boolean[][] visited = new boolean[m][n];
-        StringBuilder str = new StringBuilder();
+        Node node = root;
 
         for ( int i=0; i<m; i++ ) {
             for ( int j=0; j<n; j++ ) {
-                if ( node.contains(board[i][j]) ) {
-                    dfs(i, j, board, node, str, result);
+                char ch = board[i][j];
+                if ( node.containsKey(ch) ) {
+                    dfs(i, j, str, board, visited, node.get(ch), result);
                 }
             }
         }
-        List<String> mainRes = new ArrayList<>();
-        for ( String res : result ) mainRes.add(res);
-        return mainRes;
+
+        return result;
     }
 }
-
-
-
-
-
-
-
-// ["eath","ea","eat","eain", "oath"]
-// construct Trie , intiall add all to Trie
-// add all nodes with starting char into queue
-// now in four directions check if any of its char is in the subsequest node, if mutiple?
