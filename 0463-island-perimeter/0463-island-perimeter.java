a3c5add2618844c6a49b9e15195a8181a71c2856
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-12-14 09:58:08 -0800

    Time: 4 ms (99.89%), Space: 47.3 MB (25.21%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,34 +1,15 @@
 class Solution {
-    int perimeter = 0;
-    public void DFS(int[][] grid, boolean[][] visited, int sourceX, int sourceY, int m, int n, int[][] directions) {
-        visited[sourceX][sourceY] = true;
-        for ( int[] dir : directions ) {
-            int dx = sourceX + dir[0];
-            int dy = sourceY + dir[1];
-            if ( dx < 0 || dx >= m || dy < 0 || dy >= n || grid[dx][dy] == 0 ) {
-                perimeter++;
-                continue;
-            }
-            if ( visited[dx][dy] ) continue;
-            DFS(grid, visited, dx, dy, m, n, directions);
-        }
-    }
     public int islandPerimeter(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        boolean[][] visited = new boolean[m][n];
-        int[][] directions = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-
-        for ( int i=0; i<m; i++ ) {
-            for ( int j=0; j<n; j++ ) {
+        int perimeter = 0;
+        for ( int i=0; i<grid.length; i++ ) {
+            for ( int j=0; j<grid[0].length; j++ ) {
                 if ( grid[i][j] == 1 ) {
-                    DFS(grid, visited, i, j, m, n, directions);
-                    return perimeter;
+                    perimeter += 4;
+                    if ( i>0 && grid[i-1][j] == 1 ) perimeter -= 2;
+                    if ( j>0 && grid[i][j-1] == 1 ) perimeter -= 2;
                 }
             }
         }
-
-        return 0;
+        return perimeter;
     }
 }
