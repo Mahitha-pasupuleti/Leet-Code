c69faf76e3e321e4e414bfb41fc249bc37693875
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-12-14 17:48:57 -0800

    Time: 436 ms (5.07%), Space: 237.1 MB (8.3%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,46 +1,21 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        List<Integer> result = new ArrayList();
-        for ( int x : nums ) {
-            if ( result.isEmpty() || result.get(result.size()-1) < x ) {
-                result.add(x);
-            } else {
-                int start=0, end=result.size()-1;
-                int swapIndex = 0;
-                while ( start < end ) {
-                    int mid = (start + end)/2;
-                    if ( result.get(mid) < x ) start = mid+1;
-                    else end = mid;
-                }
-                result.set(start, x);
-            }
+    int[][] dp;
+    private int solve( int start, int prev, int[] nums ) {
+        if ( start == nums.length ) return 0;
+        if ( dp[start][prev+1] != -1 ) return dp[start][prev+1];
+        int take = 0 , notTake = 0;
+        if ( prev == -1 || nums[start] > nums[prev] ) { // current value greater than prev value or first index value
+            take = 1 + solve(start+1, start, nums);
         }
-        return result.size();
+        notTake = solve(start+1, prev, nums);
+        return dp[start][prev+1] = Math.max(take, notTake);
+    }
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        dp = new int[n+1][n+1];
+        for ( int i=0; i<=n; i++ ) {
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(0, -1, nums);
     }
 }
-
-// class Solution {
-//     // difficult problem
-//     public int lengthOfLIS(int[] nums) {
-//         int[] tails = new int[nums.length];
-//         int size = 0;
-
-//         for ( int x: nums ) {
-//             int i=0, j=size;
-
-//             while ( i != j ) {
-//                 int m = ( i + j ) / 2;
-//                 if ( tails[m] < x ) {
-//                     i = m+1;
-//                 } else {
-//                     j = m;
-//                 }
-//             }
-
-//             tails[i] = x;
-//             if ( i == size ) ++size;
-//         }
-
-//         return size;
-//     }
-// }
