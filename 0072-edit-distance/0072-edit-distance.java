53f575430837c7519573801c7a42aca97f2aeabb
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-12-14 18:44:47 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,36 +1,19 @@
 class Solution {
-    private int getMinDistance(String word1, String word2, int n, int m, int[][] dp) {
-        // if word1 is exhausted
-        if ( n <= 0 ) return m;
-        if ( m <= 0 ) return n;
-        if ( dp[n][m] != -1 ) return dp[n][m];
-
+    int[][] dp;
+    private int solve(String word1, String word2, int n, int m) {
+        if ( n == 0 ) return m;
+        if ( m == 0 ) return n;
         if ( word1.charAt(n-1) == word2.charAt(m-1) ) {
-            return dp[n][m] = getMinDistance(word1, word2, n-1, m-1, dp);
+            return solve(word1, word2, n-1, m-1);
         } else {
-            int insert = 1 + getMinDistance(word1, word2, n, m-1, dp);
-            int delete = 1 + getMinDistance(word1, word2, n-1, m, dp);
-            int replace = 1 + getMinDistance(word1, word2, n-1, m-1, dp);
-            // int minVal = 0;
-            // if ( insert < delete ) {
-            //     if ( insert < replace ) minVal = insert;
-            //     else minVal = replace;
-            // } else {
-            //     if ( delete < replace ) minVal = delete;
-            //     else minVal = replace;
-            // }
-            return dp[n][m] = Math.min(insert, Math.min(delete, replace));
+            int insert = 1 + solve(word1, word2, n, m-1); // insert
+            int delete = 1 + solve(word1, word2, n-1, m);
+            int replace = 1 + solve(word1, word2, n-1, m-1);
+            return Math.min(insert, Math.min(delete, replace));
         }
     }
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-
-        int[][] dp = new int[n+1][m+1];
-        for ( int i=0; i<=n; i++ ) {
-            Arrays.fill(dp[i], -1);
-        }
         
-        return getMinDistance(word1, word2, n, m, dp);
+        return solve(word1, word2, word1.length(), word2.length());
     }
 }
