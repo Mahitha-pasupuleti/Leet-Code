974c526b1dd84ea98d561928ae2f082160b84c1f
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-12-14 17:51:13 -0800

    Time: 20 ms (91.83%), Space: 75 MB (19.55%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,29 +1,26 @@
 class Solution {
-    // find a subarray with sum/2
-    // corner cases
-    // [1]
     Boolean[][] dp;
-    private boolean solve(int start, int[] nums, int amount) {
-        if ( amount == 0 ) return true;
-        if ( start == nums.length ) return false;
-
-        if ( dp[start][amount] != null ) return dp[start][amount];
-
-        if ( nums[start] <= amount ) {
-            return dp[start][amount] = solve(start+1, nums, amount-nums[start]) || solve(start+1, nums, amount);
+    private boolean solve(int start, int target, int[] nums) {
+        if ( target == 0 ) return true;
+        if ( target < 0 || start == nums.length ) return false;
+        if ( dp[start][target] != null ) return dp[start][target];
+        if ( nums[start] <= target ) {
+            return dp[start][target] = solve(start+1, target-nums[start], nums) || solve(start+1, target, nums);
         } else {
-            return dp[start][amount] = solve(start+1, nums, amount);
+            return dp[start][target] = solve(start+1, target, nums);
         }
     }
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for ( int num : nums ) sum += num;
-        // if sum is odd in first place, then there is no partion present
+        for ( int num : nums ) {
+            sum += num;
+        }
         if ( sum % 2 != 0 ) return false;
 
-        dp = new Boolean[nums.length + 1][sum/2 + 1];
+        dp = new Boolean[nums.length+1][(sum/2)+1];
 
-        // find subarray with given sum
-        return solve(0, nums, sum/2);
+        return solve(0, sum/2, nums);
     }
 }
+
+// find subset with sum/2
