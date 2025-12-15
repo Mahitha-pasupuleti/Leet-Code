590c042837cc2fbfaea31d3e7793b41ac30829a1
Mahitha Pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2025-12-14 17:47:12 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,20 +1,5 @@
 class Solution {
-    Boolean[] dp;
-    private boolean solve(int start, String s, List<String> wordDict) {
-        if ( start >= s.length() ) return true;
-        if ( dp[start] != null ) return dp[start];
-        for ( String word : wordDict ) {
-            if ( start + word.length() > s.length() ) continue;
-            String wordOfS = s.substring(start, start + word.length());
-            if ( wordOfS.equals(word) ) {
-                dp[start] = solve(start + word.length(), s, wordDict);
-                if ( dp[start] ) return true;
-            }
-        }
-        return false;
-    }
     public boolean wordBreak(String s, List<String> wordDict) {
-        dp = new Boolean[s.length()];
-        return solve(0, s, wordDict);
+        
     }
 }
