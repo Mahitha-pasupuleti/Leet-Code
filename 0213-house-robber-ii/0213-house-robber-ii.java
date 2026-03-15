2b2d648b4d2ceb7a6d2e2109405342d3b9e7548f
Mahitha Pasupuleti <mahithapasupuleti23@gmail.com>
2026-03-14 21:40:17 -0700

    Time: 0 ms (100%), Space: 42.8 MB (57.02%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,22 +1,23 @@
 class Solution {
     int[] dp;
-    private int solve(int start, int end, int[] nums ) {
-        if ( start >= end ) return 0;
-        if ( dp[start] != -1 ) return dp[start];
-        int pick = nums[start] + solve(start+2, end, nums); // picked first house
-        int notPick = solve(start+1, end, nums); // not picked first house
-        return dp[start] = Math.max( pick, notPick );
+    private int solve(int index, int end, int[] nums) {
+        if ( index >= end ) return 0;
+        if ( dp[index] != -1 ) return dp[index];
+        int pick = nums[index] + solve(index+2, end, nums);
+        int notPick = solve(index+1, end, nums);
+        return dp[index] = Math.max(pick, notPick);
     }
     public int rob(int[] nums) {
+        if ( nums.length == 1 ) return nums[0];
         int n = nums.length;
-        if ( n == 1 ) return nums[0];
         dp = new int[n+1];
 
         Arrays.fill(dp, -1);
-        int withZero = solve(0, n-1, nums);
+        int first = solve(0, n-1, nums);
 
         Arrays.fill(dp, -1);
-        int withoutZero = solve(1, n, nums);
-        return Math.max( withZero, withoutZero );
+        int second = solve(1, n, nums);
+
+        return Math.max( first, second );
     }
 }
