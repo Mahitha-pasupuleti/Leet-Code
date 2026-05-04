9919bceefb7edcc6298e348401c391d04acef269
Mahitha Pasupuleti <mahithapasupuleti23@gmail.com>
2026-05-03 20:05:00 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,26 +1,41 @@
 class Solution {
     public String minWindow(String s, String t) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i=0; i<t.length(); i++ ) map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0)+1);
+        if ( t.length() > s.length() ) return "";
 
-        int count = 0, minLen = Integer.MAX_VALUE, start = 0, end = 0, validStart = -1;
+        Map<Character, Integer> tMap = new HashMap<>();
+        for ( int i=0; i<t.length(); i++ ) {
+            char ch = t.charAt(i);
+            tMap.put(ch, tMap.getOrDefault(ch, 0) + 1);
+        }
+
+        int count = 0, start = 0, end = 0, startIndex = 0, minLen = Integer.MAX_VALUE;
+
         while ( end < s.length() ) {
-            if ( map.containsKey(s.charAt(end)) ) {
-                if ( map.get(s.charAt(end)) > 0 ) count++;
-                int value = map.get(s.charAt(end));
-                map.put(s.charAt(end), --value);
+            char endCh = s.charAt(end);
+
+            if (tMap.containsKey(endCh) && tMap.get(endCh) > 0) {
+                count++;
+                tMap.put(endCh, tMap.get(endCh) - 1);
             }
+
+            // if (tMap.containsKey(endCh)) {
+            //     tMap.put(endCh, tMap.get(endCh) - 1);
+            // }
 
             while ( count == t.length() ) {
                 if ( minLen > end - start + 1 ) {
                     minLen = end - start + 1;
-                    validStart = start;
+                    startIndex = start;
                 }
-                if ( map.containsKey(s.charAt(start)) ) {
-                    int value = map.get(s.charAt(start));
-                    map.put(s.charAt(start), ++value);
+                
+                // remove start
+                char startCh = s.charAt(start);
+                if ( tMap.containsKey(startCh) ) {
+                    tMap.put(startCh, tMap.get(startCh) + 1);
+                    int value = tMap.get(startCh);
                     if ( value > 0 ) count--;
                 }
+
                 start++;
             }
 
@@ -29,6 +44,6 @@ class Solution {
 
         if ( minLen == Integer.MAX_VALUE ) return "";
 
-        return s.substring(validStart, validStart + minLen);
+        return s.substring(startIndex, startIndex + minLen);
     }
 }
